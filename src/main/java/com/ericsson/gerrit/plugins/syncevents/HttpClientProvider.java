cf705c7e3e8b7aa3c1f633ef00c32a5fd695b5c9
Hugo Arès <hugo.ares@ericsson.com>
2016-07-14 08:04:28 -0400

    Stop retrying if thread is interrupted
    InterruptedException should always be honored.
    
    Change-Id: I64bfdb3840bf4fff438b88712dbd1400bd507418

--- src/main/java/com/ericsson/gerrit/plugins/syncevents/HttpClientProvider.java
+++ src/main/java/com/ericsson/gerrit/plugins/syncevents/HttpClientProvider.java
@@ -104,7 +104,8 @@ class HttpClientProvider implements Provider<CloseableHttpClient> {
         try {
           Thread.sleep(cfg.getRetryInterval());
         } catch (InterruptedException e) {
-          log.debug("Ignoring InterruptedException", e);
+          Thread.currentThread().interrupt();
+          return false;
         }
         return true;
       }
