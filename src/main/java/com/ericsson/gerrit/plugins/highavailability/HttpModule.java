GerritCodeReview/plugins_multi-master

Reorganize classes in sub-packages
Change-Id: I12ec195e94b13e4e68702b99823dd1d51cc67766

src/main/java/com/ericsson/gerrit/plugins/multimaster/HttpModule.java
@@ -16,9 +16,11 @@ package com.ericsson.gerrit.plugins.multimaster;
 
 import com.google.gerrit.httpd.plugins.HttpPluginModule;
 
+import com.ericsson.gerrit.plugins.multimaster.forwarder.rest.EventForwarderServletModule;
+
 class HttpModule extends HttpPluginModule {
   @Override
   protected void configureServlets() {
-    serveRegex("/index/\\d+$").with(SyncIndexRestApiServlet.class);
+    install(new EventForwarderServletModule());
   }
 }

src/main/java/com/ericsson/gerrit/plugins/multimaster/forwarder/rest/HttpResponseHandler.java
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.multimaster;
+package com.ericsson.gerrit.plugins.multimaster.forwarder.rest;
 
 import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
 
-import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler.HttpResult;
+import com.ericsson.gerrit.plugins.multimaster.forwarder.rest.HttpResponseHandler.HttpResult;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;

src/main/java/com/ericsson/gerrit/plugins/multimaster/index/IndexModule.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2015 Ericsson
+// Copyright (C) 2017 Ericsson
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,20 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.multimaster;
+package com.ericsson.gerrit.plugins.multimaster.index;
 
+import com.google.gerrit.extensions.events.ChangeIndexedListener;
+import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.lifecycle.LifecycleModule;
-import com.google.inject.Scopes;
 
-import com.ericsson.gerrit.plugins.multimaster.forwarder.rest.RestEventForwarderModule;
-import com.ericsson.gerrit.plugins.multimaster.index.IndexModule;
+import java.util.concurrent.Executor;
 
-class Module extends LifecycleModule {
+public class IndexModule extends LifecycleModule {
 
   @Override
   protected void configure() {
-    bind(Configuration.class).in(Scopes.SINGLETON);
-    install(new RestEventForwarderModule());
-    install(new IndexModule());
+    bind(Executor.class)
+        .annotatedWith(IndexExecutor.class)
+        .toProvider(IndexExecutorProvider.class);
+    listener().to(IndexExecutorProvider.class);
+    DynamicSet.bind(binder(), ChangeIndexedListener.class).to(
+        IndexEventHandler.class);
   }
 }

src/test/java/com/ericsson/gerrit/plugins/multimaster/forwarder/rest/HttpSessionTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.multimaster;
+package com.ericsson.gerrit.plugins.multimaster.forwarder.rest;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.delete;
@@ -23,9 +23,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.ericsson.gerrit.plugins.multimaster.Configuration;
-import com.ericsson.gerrit.plugins.multimaster.HttpClientProvider;
-import com.ericsson.gerrit.plugins.multimaster.HttpSession;
-import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler.HttpResult;
+import com.ericsson.gerrit.plugins.multimaster.forwarder.rest.HttpClientProvider;
+import com.ericsson.gerrit.plugins.multimaster.forwarder.rest.HttpSession;
+import com.ericsson.gerrit.plugins.multimaster.forwarder.rest.HttpResponseHandler.HttpResult;
 import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;

src/test/java/com/ericsson/gerrit/plugins/multimaster/forwarder/rest/IndexRestApiServletTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.multimaster;
+package com.ericsson.gerrit.plugins.multimaster.forwarder.rest;
 
 import static javax.servlet.http.HttpServletResponse.SC_CONFLICT;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
@@ -34,7 +34,7 @@ import com.google.gwtorm.server.OrmException;
 import com.google.gwtorm.server.SchemaFactory;
 import com.google.gwtorm.server.StandardKeyEncoder;
 
-import com.ericsson.gerrit.plugins.multimaster.SyncIndexRestApiServlet;
+import com.ericsson.gerrit.plugins.multimaster.forwarder.rest.IndexRestApiServlet;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -49,7 +49,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RunWith(MockitoJUnitRunner.class)
-public class SyncIndexRestApiServletTest {
+public class IndexRestApiServletTest {
   private static final boolean CHANGE_EXISTS = true;
   private static final boolean CHANGE_DOES_NOT_EXIST = false;
   private static final boolean DO_NOT_THROW_IO_EXCEPTION = false;
@@ -70,7 +70,7 @@ public class SyncIndexRestApiServletTest {
   private HttpServletResponse rsp;
   private Change.Id id;
   private Change change;
-  private SyncIndexRestApiServlet syncIndexRestApiServlet;
+  private IndexRestApiServlet indexRestApiServlet;
 
   @BeforeClass
   public static void setup() {
@@ -79,8 +79,7 @@ public class SyncIndexRestApiServletTest {
 
   @Before
   public void setUpMocks() {
-    syncIndexRestApiServlet =
-        new SyncIndexRestApiServlet(indexer, schemaFactory);
+    indexRestApiServlet = new IndexRestApiServlet(indexer, schemaFactory);
     id = Change.Id.parse(CHANGE_NUMBER);
     when(req.getPathInfo()).thenReturn("/index/" + CHANGE_NUMBER);
     change = new Change(null, id, null, null, TimeUtil.nowTs());
@@ -89,7 +88,7 @@ public class SyncIndexRestApiServletTest {
   @Test
   public void changeIsIndexed() throws Exception {
     setupPostMocks(CHANGE_EXISTS);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(indexer, times(1)).index(db, change);
     verify(rsp).setStatus(SC_NO_CONTENT);
   }
@@ -97,7 +96,7 @@ public class SyncIndexRestApiServletTest {
   @Test
   public void changeToIndexDoNotExist() throws Exception {
     setupPostMocks(CHANGE_DOES_NOT_EXIST);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(indexer, times(1)).delete(id);
     verify(rsp).setStatus(SC_NO_CONTENT);
   }
@@ -105,7 +104,7 @@ public class SyncIndexRestApiServletTest {
   @Test
   public void schemaThrowsExceptionWhenLookingUpForChange() throws Exception {
     setupPostMocks(CHANGE_EXISTS, THROW_ORM_EXCEPTION);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(rsp).sendError(SC_NOT_FOUND, "Error trying to find a change \n");
   }
 
@@ -113,13 +112,13 @@ public class SyncIndexRestApiServletTest {
   public void indexerThrowsIOExceptionTryingToIndexChange() throws Exception {
     setupPostMocks(CHANGE_EXISTS, DO_NOT_THROW_ORM_EXCEPTION,
         THROW_IO_EXCEPTION);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(rsp).sendError(SC_CONFLICT, "io-error");
   }
 
   @Test
   public void changeIsDeletedFromIndex() throws Exception {
-    syncIndexRestApiServlet.doDelete(req, rsp);
+    indexRestApiServlet.doDelete(req, rsp);
     verify(indexer, times(1)).delete(id);
     verify(rsp).setStatus(SC_NO_CONTENT);
   }
@@ -127,7 +126,7 @@ public class SyncIndexRestApiServletTest {
   @Test
   public void indexerThrowsExceptionTryingToDeleteChange() throws Exception {
     doThrow(new IOException("io-error")).when(indexer).delete(id);
-    syncIndexRestApiServlet.doDelete(req, rsp);
+    indexRestApiServlet.doDelete(req, rsp);
     verify(rsp).sendError(SC_CONFLICT, "io-error");
   }
 
@@ -136,7 +135,7 @@ public class SyncIndexRestApiServletTest {
     doThrow(new IOException("someError")).when(rsp).sendError(SC_NOT_FOUND,
         "Error trying to find a change \n");
     setupPostMocks(CHANGE_EXISTS, THROW_ORM_EXCEPTION);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(rsp).sendError(SC_NOT_FOUND, "Error trying to find a change \n");
     verifyZeroInteractions(indexer);
   }

GerritCodeReview/plugins_multi-site

src/main/java/com/ericsson/gerrit/plugins/highavailability/HttpModule.java
@@ -16,9 +16,11 @@ package com.ericsson.gerrit.plugins.highavailability;
 
 import com.google.gerrit.httpd.plugins.HttpPluginModule;
 
+import com.ericsson.gerrit.plugins.highavailability.forwarder.rest.EventForwarderServletModule;
+
 class HttpModule extends HttpPluginModule {
   @Override
   protected void configureServlets() {
-    serveRegex("/index/\\d+$").with(SyncIndexRestApiServlet.class);
+    install(new EventForwarderServletModule());
   }
 }

src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/HttpResponseHandler.java
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.highavailability;
+package com.ericsson.gerrit.plugins.highavailability.forwarder.rest;
 
 import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
 
-import com.ericsson.gerrit.plugins.highavailability.HttpResponseHandler.HttpResult;
+import com.ericsson.gerrit.plugins.highavailability.forwarder.rest.HttpResponseHandler.HttpResult;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;

src/main/java/com/ericsson/gerrit/plugins/highavailability/index/IndexModule.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2015 Ericsson
+// Copyright (C) 2017 Ericsson
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,20 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.highavailability;
+package com.ericsson.gerrit.plugins.highavailability.index;
 
+import com.google.gerrit.extensions.events.ChangeIndexedListener;
+import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.lifecycle.LifecycleModule;
-import com.google.inject.Scopes;
 
-import com.ericsson.gerrit.plugins.highavailability.forwarder.rest.RestEventForwarderModule;
-import com.ericsson.gerrit.plugins.highavailability.index.IndexModule;
+import java.util.concurrent.Executor;
 
-class Module extends LifecycleModule {
+public class IndexModule extends LifecycleModule {
 
   @Override
   protected void configure() {
-    bind(Configuration.class).in(Scopes.SINGLETON);
-    install(new RestEventForwarderModule());
-    install(new IndexModule());
+    bind(Executor.class)
+        .annotatedWith(IndexExecutor.class)
+        .toProvider(IndexExecutorProvider.class);
+    listener().to(IndexExecutorProvider.class);
+    DynamicSet.bind(binder(), ChangeIndexedListener.class).to(
+        IndexEventHandler.class);
   }
 }

src/test/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/HttpSessionTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.highavailability;
+package com.ericsson.gerrit.plugins.highavailability.forwarder.rest;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.delete;
@@ -22,7 +22,8 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.ericsson.gerrit.plugins.highavailability.HttpResponseHandler.HttpResult;
+import com.ericsson.gerrit.plugins.highavailability.Configuration;
+import com.ericsson.gerrit.plugins.highavailability.forwarder.rest.HttpResponseHandler.HttpResult;
 import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;

src/test/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/IndexRestApiServletTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.highavailability;
+package com.ericsson.gerrit.plugins.highavailability.forwarder.rest;
 
 import static javax.servlet.http.HttpServletResponse.SC_CONFLICT;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
@@ -34,8 +34,6 @@ import com.google.gwtorm.server.OrmException;
 import com.google.gwtorm.server.SchemaFactory;
 import com.google.gwtorm.server.StandardKeyEncoder;
 
-import com.ericsson.gerrit.plugins.highavailability.SyncIndexRestApiServlet;
-
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -49,7 +47,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RunWith(MockitoJUnitRunner.class)
-public class SyncIndexRestApiServletTest {
+public class IndexRestApiServletTest {
   private static final boolean CHANGE_EXISTS = true;
   private static final boolean CHANGE_DOES_NOT_EXIST = false;
   private static final boolean DO_NOT_THROW_IO_EXCEPTION = false;
@@ -70,7 +68,7 @@ public class SyncIndexRestApiServletTest {
   private HttpServletResponse rsp;
   private Change.Id id;
   private Change change;
-  private SyncIndexRestApiServlet syncIndexRestApiServlet;
+  private IndexRestApiServlet indexRestApiServlet;
 
   @BeforeClass
   public static void setup() {
@@ -79,8 +77,7 @@ public class SyncIndexRestApiServletTest {
 
   @Before
   public void setUpMocks() {
-    syncIndexRestApiServlet =
-        new SyncIndexRestApiServlet(indexer, schemaFactory);
+    indexRestApiServlet = new IndexRestApiServlet(indexer, schemaFactory);
     id = Change.Id.parse(CHANGE_NUMBER);
     when(req.getPathInfo()).thenReturn("/index/" + CHANGE_NUMBER);
     change = new Change(null, id, null, null, TimeUtil.nowTs());
@@ -89,7 +86,7 @@ public class SyncIndexRestApiServletTest {
   @Test
   public void changeIsIndexed() throws Exception {
     setupPostMocks(CHANGE_EXISTS);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(indexer, times(1)).index(db, change);
     verify(rsp).setStatus(SC_NO_CONTENT);
   }
@@ -97,7 +94,7 @@ public class SyncIndexRestApiServletTest {
   @Test
   public void changeToIndexDoNotExist() throws Exception {
     setupPostMocks(CHANGE_DOES_NOT_EXIST);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(indexer, times(1)).delete(id);
     verify(rsp).setStatus(SC_NO_CONTENT);
   }
@@ -105,7 +102,7 @@ public class SyncIndexRestApiServletTest {
   @Test
   public void schemaThrowsExceptionWhenLookingUpForChange() throws Exception {
     setupPostMocks(CHANGE_EXISTS, THROW_ORM_EXCEPTION);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(rsp).sendError(SC_NOT_FOUND, "Error trying to find a change \n");
   }
 
@@ -113,13 +110,13 @@ public class SyncIndexRestApiServletTest {
   public void indexerThrowsIOExceptionTryingToIndexChange() throws Exception {
     setupPostMocks(CHANGE_EXISTS, DO_NOT_THROW_ORM_EXCEPTION,
         THROW_IO_EXCEPTION);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(rsp).sendError(SC_CONFLICT, "io-error");
   }
 
   @Test
   public void changeIsDeletedFromIndex() throws Exception {
-    syncIndexRestApiServlet.doDelete(req, rsp);
+    indexRestApiServlet.doDelete(req, rsp);
     verify(indexer, times(1)).delete(id);
     verify(rsp).setStatus(SC_NO_CONTENT);
   }
@@ -127,7 +124,7 @@ public class SyncIndexRestApiServletTest {
   @Test
   public void indexerThrowsExceptionTryingToDeleteChange() throws Exception {
     doThrow(new IOException("io-error")).when(indexer).delete(id);
-    syncIndexRestApiServlet.doDelete(req, rsp);
+    indexRestApiServlet.doDelete(req, rsp);
     verify(rsp).sendError(SC_CONFLICT, "io-error");
   }
 
@@ -136,7 +133,7 @@ public class SyncIndexRestApiServletTest {
     doThrow(new IOException("someError")).when(rsp).sendError(SC_NOT_FOUND,
         "Error trying to find a change \n");
     setupPostMocks(CHANGE_EXISTS, THROW_ORM_EXCEPTION);
-    syncIndexRestApiServlet.doPost(req, rsp);
+    indexRestApiServlet.doPost(req, rsp);
     verify(rsp).sendError(SC_NOT_FOUND, "Error trying to find a change \n");
     verifyZeroInteractions(indexer);
   }