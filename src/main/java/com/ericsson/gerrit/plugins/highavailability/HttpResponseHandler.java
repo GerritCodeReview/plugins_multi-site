GerritCodeReview/plugins_multi-master

Rename IndexResponseHandler and IndexResult
Both classes are not specific to Index and will be reused by the classes
of the 2 other plugins to be merged in this one: evict-cache and
sync-events.

Change-Id: I8fd554fda96b3e65f6b85620192fb31b9f2ebddc

src/main/java/com/ericsson/gerrit/plugins/multimaster/HttpResponseHandler.java
@@ -16,7 +16,7 @@ package com.ericsson.gerrit.plugins.multimaster;
 
 import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
 
-import com.ericsson.gerrit.plugins.multimaster.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler.HttpResult;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -27,13 +27,13 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-class IndexResponseHandler implements ResponseHandler<IndexResult> {
+class HttpResponseHandler implements ResponseHandler<HttpResult> {
 
-  static class IndexResult {
+  static class HttpResult {
     private boolean successful;
     private String message;
 
-    IndexResult(boolean successful, String message) {
+    HttpResult(boolean successful, String message) {
       this.successful = successful;
       this.message = message;
     }
@@ -47,12 +47,12 @@ class IndexResponseHandler implements ResponseHandler<IndexResult> {
     }
   }
 
-  private static final Logger log = LoggerFactory
-      .getLogger(IndexResponseHandler.class);
+  private static final Logger log =
+      LoggerFactory.getLogger(HttpResponseHandler.class);
 
   @Override
-  public IndexResult handleResponse(HttpResponse response) {
-    return new IndexResult(isSuccessful(response), parseResponse(response));
+  public HttpResult handleResponse(HttpResponse response) {
+    return new HttpResult(isSuccessful(response), parseResponse(response));
   }
 
   private boolean isSuccessful(HttpResponse response) {

src/main/java/com/ericsson/gerrit/plugins/multimaster/HttpSession.java
@@ -16,7 +16,7 @@ package com.ericsson.gerrit.plugins.multimaster;
 
 import com.google.inject.Inject;
 
-import com.ericsson.gerrit.plugins.multimaster.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler.HttpResult;
 
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPost;
@@ -35,13 +35,13 @@ class HttpSession {
     this.url = url;
   }
 
-  IndexResult post(String endpoint) throws IOException {
+  HttpResult post(String endpoint) throws IOException {
     return httpClient.execute(new HttpPost(url + endpoint),
-        new IndexResponseHandler());
+        new HttpResponseHandler());
   }
 
-  IndexResult delete(String endpoint) throws IOException {
+  HttpResult delete(String endpoint) throws IOException {
     return httpClient.execute(new HttpDelete(url + endpoint),
-        new IndexResponseHandler());
+        new HttpResponseHandler());
   }
 }

src/main/java/com/ericsson/gerrit/plugins/multimaster/RestSession.java
@@ -18,7 +18,7 @@ import com.google.common.base.Joiner;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.inject.Inject;
 
-import com.ericsson.gerrit.plugins.multimaster.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler.HttpResult;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +39,7 @@ class RestSession {
 
   boolean index(int changeId) {
     try {
-      IndexResult result = httpSession.post(buildEndpoint(changeId));
+      HttpResult result = httpSession.post(buildEndpoint(changeId));
       if (result.isSuccessful()) {
         return true;
       }
@@ -53,7 +53,7 @@ class RestSession {
 
   boolean deleteFromIndex(int changeId) {
     try {
-      IndexResult result = httpSession.delete(buildEndpoint(changeId));
+      HttpResult result = httpSession.delete(buildEndpoint(changeId));
       if (result.isSuccessful()) {
         return true;
       }

src/test/java/com/ericsson/gerrit/plugins/multimaster/HttpResponseHandlerTest.java
@@ -18,8 +18,8 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.ericsson.gerrit.plugins.multimaster.IndexResponseHandler;
-import com.ericsson.gerrit.plugins.multimaster.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler;
+import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler.HttpResult;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
@@ -29,23 +29,23 @@ import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
 
-public class IndexResponseHandlerTest {
+public class HttpResponseHandlerTest {
   private static final int ERROR = 400;
   private static final int OK = 204;
   private static final String EMPTY_ENTITY = "";
   private static final String ERROR_ENTITY = "Error";
 
-  private IndexResponseHandler handler;
+  private HttpResponseHandler handler;
 
   @Before
   public void setUp() throws Exception {
-    handler = new IndexResponseHandler();
+    handler = new HttpResponseHandler();
   }
 
   @Test
   public void testIsSuccessful() throws Exception {
     HttpResponse response = setupMocks(OK, EMPTY_ENTITY);
-    IndexResult result = handler.handleResponse(response);
+    HttpResult result = handler.handleResponse(response);
     assertThat(result.isSuccessful()).isTrue();
     assertThat(result.getMessage()).isEmpty();
   }
@@ -53,7 +53,7 @@ public class IndexResponseHandlerTest {
   @Test
   public void testIsNotSuccessful() throws Exception {
     HttpResponse response = setupMocks(ERROR, ERROR_ENTITY);
-    IndexResult result = handler.handleResponse(response);
+    HttpResult result = handler.handleResponse(response);
     assertThat(result.isSuccessful()).isFalse();
     assertThat(result.getMessage()).contains(ERROR_ENTITY);
   }

src/test/java/com/ericsson/gerrit/plugins/multimaster/HttpSessionTest.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.when;
 import com.ericsson.gerrit.plugins.multimaster.Configuration;
 import com.ericsson.gerrit.plugins.multimaster.HttpClientProvider;
 import com.ericsson.gerrit.plugins.multimaster.HttpSession;
-import com.ericsson.gerrit.plugins.multimaster.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler.HttpResult;
 import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;
@@ -98,7 +98,7 @@ public class HttpSessionTest {
     wireMockRule.givenThat(post(urlEqualTo(ENDPOINT))
         .willReturn(aResponse().withStatus(UNAUTHORIZED).withBody(expected)));
 
-    IndexResult result = httpSession.post(ENDPOINT);
+    HttpResult result = httpSession.post(ENDPOINT);
     assertThat(result.isSuccessful()).isFalse();
     assertThat(result.getMessage()).isEqualTo(expected);
   }
@@ -109,7 +109,7 @@ public class HttpSessionTest {
     wireMockRule.givenThat(post(urlEqualTo(ENDPOINT))
         .willReturn(aResponse().withStatus(NOT_FOUND).withBody(expected)));
 
-    IndexResult result = httpSession.post(ENDPOINT);
+    HttpResult result = httpSession.post(ENDPOINT);
     assertThat(result.isSuccessful()).isFalse();
     assertThat(result.getMessage()).isEqualTo(expected);
   }
@@ -131,7 +131,7 @@ public class HttpSessionTest {
     wireMockRule.givenThat(post(urlEqualTo(ENDPOINT))
         .willReturn(aResponse().withStatus(ERROR).withBody(ERROR_MESSAGE)));
 
-    IndexResult result = httpSession.post(ENDPOINT);
+    HttpResult result = httpSession.post(ENDPOINT);
     assertThat(result.isSuccessful()).isFalse();
     assertThat(result.getMessage()).isEqualTo(ERROR_MESSAGE);
   }

src/test/java/com/ericsson/gerrit/plugins/multimaster/RestSessionTest.java
@@ -23,7 +23,7 @@ import com.google.common.base.Joiner;
 
 import com.ericsson.gerrit.plugins.multimaster.HttpSession;
 import com.ericsson.gerrit.plugins.multimaster.RestSession;
-import com.ericsson.gerrit.plugins.multimaster.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.multimaster.HttpResponseHandler.HttpResult;
 
 import org.junit.Test;
 
@@ -92,7 +92,7 @@ public class RestSessionTest {
         doThrow(new IOException()).when(httpSession).delete(request);
       }
     } else {
-      IndexResult result = new IndexResult(isOperationSuccessful, msg);
+      HttpResult result = new HttpResult(isOperationSuccessful, msg);
       if (operation.equals(INDEX_OP)) {
         when(httpSession.post(request)).thenReturn(result);
       } else {

GerritCodeReview/plugins_multi-site

src/main/java/com/ericsson/gerrit/plugins/highavailability/HttpResponseHandler.java
@@ -16,7 +16,7 @@ package com.ericsson.gerrit.plugins.highavailability;
 
 import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
 
-import com.ericsson.gerrit.plugins.highavailability.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.highavailability.HttpResponseHandler.HttpResult;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -27,13 +27,13 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-class IndexResponseHandler implements ResponseHandler<IndexResult> {
+class HttpResponseHandler implements ResponseHandler<HttpResult> {
 
-  static class IndexResult {
+  static class HttpResult {
     private boolean successful;
     private String message;
 
-    IndexResult(boolean successful, String message) {
+    HttpResult(boolean successful, String message) {
       this.successful = successful;
       this.message = message;
     }
@@ -47,12 +47,12 @@ class IndexResponseHandler implements ResponseHandler<IndexResult> {
     }
   }
 
-  private static final Logger log = LoggerFactory
-      .getLogger(IndexResponseHandler.class);
+  private static final Logger log =
+      LoggerFactory.getLogger(HttpResponseHandler.class);
 
   @Override
-  public IndexResult handleResponse(HttpResponse response) {
-    return new IndexResult(isSuccessful(response), parseResponse(response));
+  public HttpResult handleResponse(HttpResponse response) {
+    return new HttpResult(isSuccessful(response), parseResponse(response));
   }
 
   private boolean isSuccessful(HttpResponse response) {

src/main/java/com/ericsson/gerrit/plugins/highavailability/HttpSession.java
@@ -16,7 +16,7 @@ package com.ericsson.gerrit.plugins.highavailability;
 
 import com.google.inject.Inject;
 
-import com.ericsson.gerrit.plugins.highavailability.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.highavailability.HttpResponseHandler.HttpResult;
 
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPost;
@@ -35,13 +35,13 @@ class HttpSession {
     this.url = url;
   }
 
-  IndexResult post(String endpoint) throws IOException {
+  HttpResult post(String endpoint) throws IOException {
     return httpClient.execute(new HttpPost(url + endpoint),
-        new IndexResponseHandler());
+        new HttpResponseHandler());
   }
 
-  IndexResult delete(String endpoint) throws IOException {
+  HttpResult delete(String endpoint) throws IOException {
     return httpClient.execute(new HttpDelete(url + endpoint),
-        new IndexResponseHandler());
+        new HttpResponseHandler());
   }
 }

src/main/java/com/ericsson/gerrit/plugins/highavailability/RestSession.java
@@ -18,7 +18,7 @@ import com.google.common.base.Joiner;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.inject.Inject;
 
-import com.ericsson.gerrit.plugins.highavailability.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.highavailability.HttpResponseHandler.HttpResult;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +39,7 @@ class RestSession {
 
   boolean index(int changeId) {
     try {
-      IndexResult result = httpSession.post(buildEndpoint(changeId));
+      HttpResult result = httpSession.post(buildEndpoint(changeId));
       if (result.isSuccessful()) {
         return true;
       }
@@ -53,7 +53,7 @@ class RestSession {
 
   boolean deleteFromIndex(int changeId) {
     try {
-      IndexResult result = httpSession.delete(buildEndpoint(changeId));
+      HttpResult result = httpSession.delete(buildEndpoint(changeId));
       if (result.isSuccessful()) {
         return true;
       }

src/test/java/com/ericsson/gerrit/plugins/highavailability/HttpResponseHandlerTest.java
@@ -18,8 +18,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.ericsson.gerrit.plugins.highavailability.IndexResponseHandler;
-import com.ericsson.gerrit.plugins.highavailability.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.highavailability.HttpResponseHandler.HttpResult;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
@@ -29,23 +28,23 @@ import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
 
-public class IndexResponseHandlerTest {
+public class HttpResponseHandlerTest {
   private static final int ERROR = 400;
   private static final int OK = 204;
   private static final String EMPTY_ENTITY = "";
   private static final String ERROR_ENTITY = "Error";
 
-  private IndexResponseHandler handler;
+  private HttpResponseHandler handler;
 
   @Before
   public void setUp() throws Exception {
-    handler = new IndexResponseHandler();
+    handler = new HttpResponseHandler();
   }
 
   @Test
   public void testIsSuccessful() throws Exception {
     HttpResponse response = setupMocks(OK, EMPTY_ENTITY);
-    IndexResult result = handler.handleResponse(response);
+    HttpResult result = handler.handleResponse(response);
     assertThat(result.isSuccessful()).isTrue();
     assertThat(result.getMessage()).isEmpty();
   }
@@ -53,7 +52,7 @@ public class IndexResponseHandlerTest {
   @Test
   public void testIsNotSuccessful() throws Exception {
     HttpResponse response = setupMocks(ERROR, ERROR_ENTITY);
-    IndexResult result = handler.handleResponse(response);
+    HttpResult result = handler.handleResponse(response);
     assertThat(result.isSuccessful()).isFalse();
     assertThat(result.getMessage()).contains(ERROR_ENTITY);
   }

src/test/java/com/ericsson/gerrit/plugins/highavailability/HttpSessionTest.java
@@ -22,14 +22,10 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.ericsson.gerrit.plugins.highavailability.Configuration;
-import com.ericsson.gerrit.plugins.highavailability.HttpClientProvider;
-import com.ericsson.gerrit.plugins.highavailability.HttpSession;
-import com.ericsson.gerrit.plugins.highavailability.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.highavailability.HttpResponseHandler.HttpResult;
 import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;
-
 
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -98,7 +94,7 @@ public class HttpSessionTest {
     wireMockRule.givenThat(post(urlEqualTo(ENDPOINT))
         .willReturn(aResponse().withStatus(UNAUTHORIZED).withBody(expected)));
 
-    IndexResult result = httpSession.post(ENDPOINT);
+    HttpResult result = httpSession.post(ENDPOINT);
     assertThat(result.isSuccessful()).isFalse();
     assertThat(result.getMessage()).isEqualTo(expected);
   }
@@ -109,7 +105,7 @@ public class HttpSessionTest {
     wireMockRule.givenThat(post(urlEqualTo(ENDPOINT))
         .willReturn(aResponse().withStatus(NOT_FOUND).withBody(expected)));
 
-    IndexResult result = httpSession.post(ENDPOINT);
+    HttpResult result = httpSession.post(ENDPOINT);
     assertThat(result.isSuccessful()).isFalse();
     assertThat(result.getMessage()).isEqualTo(expected);
   }
@@ -131,7 +127,7 @@ public class HttpSessionTest {
     wireMockRule.givenThat(post(urlEqualTo(ENDPOINT))
         .willReturn(aResponse().withStatus(ERROR).withBody(ERROR_MESSAGE)));
 
-    IndexResult result = httpSession.post(ENDPOINT);
+    HttpResult result = httpSession.post(ENDPOINT);
     assertThat(result.isSuccessful()).isFalse();
     assertThat(result.getMessage()).isEqualTo(ERROR_MESSAGE);
   }

src/test/java/com/ericsson/gerrit/plugins/highavailability/RestSessionTest.java
@@ -21,9 +21,7 @@ import static org.mockito.Mockito.when;
 
 import com.google.common.base.Joiner;
 
-import com.ericsson.gerrit.plugins.highavailability.HttpSession;
-import com.ericsson.gerrit.plugins.highavailability.RestSession;
-import com.ericsson.gerrit.plugins.highavailability.IndexResponseHandler.IndexResult;
+import com.ericsson.gerrit.plugins.highavailability.HttpResponseHandler.HttpResult;
 
 import org.junit.Test;
 
@@ -92,7 +90,7 @@ public class RestSessionTest {
         doThrow(new IOException()).when(httpSession).delete(request);
       }
     } else {
-      IndexResult result = new IndexResult(isOperationSuccessful, msg);
+      HttpResult result = new HttpResult(isOperationSuccessful, msg);
       if (operation.equals(INDEX_OP)) {
         when(httpSession.post(request)).thenReturn(result);
       } else {