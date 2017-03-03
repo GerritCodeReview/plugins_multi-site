GerritCodeReview/plugins_multi-master

Remove last references to sync-index plugin
Change-Id: I60fc9d0d56380e3b7f9273eb558a2aa7a0153b56

src/test/java/com/ericsson/gerrit/plugins/multimaster/ConfigurationTest.java
@@ -46,7 +46,7 @@ public class ConfigurationTest {
   @Mock
   private PluginConfig configMock;
   private Configuration configuration;
-  private String pluginName = "sync-events";
+  private String pluginName = "multi-master";
 
   @Before
   public void setUp() throws Exception {

src/test/java/com/ericsson/gerrit/plugins/multimaster/forwarder/rest/HttpSessionTest.java
@@ -46,7 +46,7 @@ public class HttpSessionTest {
   private static final int NOT_FOUND = 404;
   private static final int UNAUTHORIZED = 401;
 
-  private static final String ENDPOINT = "/plugins/sync-index/index/1";
+  private static final String ENDPOINT = "/plugins/multi-master/index/1";
   private static final String ERROR_MESSAGE = "Error message";
   private static final String REQUEST_MADE = "Request made";
   private static final String SECOND_TRY = "Second try";

src/test/java/com/ericsson/gerrit/plugins/multimaster/forwarder/rest/RestEventForwarderTest.java
@@ -33,7 +33,7 @@ public class RestEventForwarderTest {
   private static final int CHANGE_NUMBER = 1;
   private static final String DELETE_OP = "delete";
   private static final String INDEX_OP = "index";
-  private static final String PLUGIN_NAME = "sync-index";
+  private static final String PLUGIN_NAME = "multi-master";
   private static final String EMPTY_MSG = "";
   private static final String ERROR_MSG = "Error";
   private static final String EXCEPTION_MSG = "Exception";

src/test/java/com/ericsson/gerrit/plugins/multimaster/index/IndexEventHandlerTest.java
@@ -40,7 +40,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndexEventHandlerTest {
-  private static final String PLUGIN_NAME = "sync-index";
+  private static final String PLUGIN_NAME = "multi-master";
   private static final int CHANGE_ID = 1;
 
   private IndexEventHandler indexEventHandler;

GerritCodeReview/plugins_multi-site

src/test/java/com/ericsson/gerrit/plugins/highavailability/ConfigurationTest.java
@@ -46,7 +46,7 @@ public class ConfigurationTest {
   @Mock
   private PluginConfig configMock;
   private Configuration configuration;
-  private String pluginName = "sync-events";
+  private String pluginName = "high-availability";
 
   @Before
   public void setUp() throws Exception {

src/test/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/HttpSessionTest.java
@@ -43,7 +43,7 @@ public class HttpSessionTest {
   private static final int NOT_FOUND = 404;
   private static final int UNAUTHORIZED = 401;
 
-  private static final String ENDPOINT = "/plugins/sync-index/index/1";
+  private static final String ENDPOINT = "/plugins/high-availability/index/1";
   private static final String ERROR_MESSAGE = "Error message";
   private static final String REQUEST_MADE = "Request made";
   private static final String SECOND_TRY = "Second try";

src/test/java/com/ericsson/gerrit/plugins/highavailability/forwarder/rest/RestEventForwarderTest.java
@@ -31,7 +31,7 @@ public class RestEventForwarderTest {
   private static final int CHANGE_NUMBER = 1;
   private static final String DELETE_OP = "delete";
   private static final String INDEX_OP = "index";
-  private static final String PLUGIN_NAME = "sync-index";
+  private static final String PLUGIN_NAME = "high-availability";
   private static final String EMPTY_MSG = "";
   private static final String ERROR_MSG = "Error";
   private static final String EXCEPTION_MSG = "Exception";

src/test/java/com/ericsson/gerrit/plugins/highavailability/index/IndexEventHandlerTest.java
@@ -39,7 +39,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndexEventHandlerTest {
-  private static final String PLUGIN_NAME = "sync-index";
+  private static final String PLUGIN_NAME = "high-availability";
   private static final int CHANGE_ID = 1;
 
   private IndexEventHandler indexEventHandler;