aa05b945191c53c5e9ffe4ef5c881effdd00aec8
Hugo Arès <hugo.ares@ericsson.com>
2017-06-07 13:05:46 -0400

    Move ForwardedAwareEventBroker to forwarder package
    Change-Id: I0db94cb3cb5c745661230bc6f8a3eebd706fa5cf

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedAwareEventBroker.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedAwareEventBroker.java
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.ericsson.gerrit.plugins.highavailability.event;
+package com.ericsson.gerrit.plugins.highavailability.forwarder;
 
-import com.ericsson.gerrit.plugins.highavailability.forwarder.Context;
 import com.google.gerrit.common.EventBroker;
 import com.google.gerrit.common.EventListener;
 import com.google.gerrit.common.UserScopedEventListener;
