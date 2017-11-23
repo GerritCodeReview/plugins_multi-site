5827ea66d878df6a936bea8c0f13b91df0422571
Eryk Szymanski <eryksz@gmail.com>
2017-11-23 18:25:54 +0100

    Add debug logging to the Configuration class
    Display strategy, skip interfaces, url and cluster name on debug level
    
    Change-Id: I63c6dfc2b78821472c9291362e082adccc06017f
    Signed-off-by: Eryk Szymanski <eryksz@gmail.com>

--- src/main/java/com/ericsson/gerrit/plugins/highavailability/Configuration.java
+++ src/main/java/com/ericsson/gerrit/plugins/highavailability/Configuration.java
@@ -237,6 +237,7 @@ public class Configuration {
 
     private PeerInfo(Config cfg) {
       strategy = cfg.getEnum(PEER_INFO_SECTION, null, STRATEGY_KEY, DEFAULT_PEER_INFO_STRATEGY);
+      log.debug("Strategy: {}", strategy.name());
     }
 
     public PeerInfoStrategy strategy() {
@@ -251,6 +252,7 @@ public class Configuration {
       url =
           trimTrailingSlash(
               Strings.nullToEmpty(cfg.getString(PEER_INFO_SECTION, STATIC_SUBSECTION, URL_KEY)));
+      log.debug("Url: {}", url);
     }
 
     public String url() {
@@ -263,6 +265,7 @@ public class Configuration {
 
     private PeerInfoJGroups(Config cfg) {
       myUrl = trimTrailingSlash(cfg.getString(PEER_INFO_SECTION, JGROUPS_SUBSECTION, MY_URL_KEY));
+      log.debug("My Url: {}", myUrl);
     }
 
     public String myUrl() {
@@ -277,7 +280,9 @@ public class Configuration {
     private JGroups(Config cfg) {
       String[] skip = cfg.getStringList(JGROUPS_SECTION, null, SKIP_INTERFACE_KEY);
       skipInterface = skip.length == 0 ? DEFAULT_SKIP_INTERFACE_LIST : ImmutableList.copyOf(skip);
+      log.debug("Skip interface(s): {}", skipInterface);
       clusterName = getString(cfg, JGROUPS_SECTION, null, CLUSTER_NAME_KEY, DEFAULT_CLUSTER_NAME);
+      log.debug("Cluster name: {}", clusterName);
     }
 
     public ImmutableList<String> skipInterface() {
