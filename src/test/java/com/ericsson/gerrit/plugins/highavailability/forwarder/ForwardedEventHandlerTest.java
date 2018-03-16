GerritCodeReview/plugins_high-availability

Rename DispatchEvent to ForwardedEventHandler
The class listening to events is called EventHandler and it uses the
Forwarder to send event to the other node. ForwardedEventHandler for the
name of the class that must handle the forwarded event is more clear
than DispatchEvent which could be confused with core EventDispatcher.

Change-Id: I2eb72946c07ae2e2237958c2edaaab5ef39b1597

src/test/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedEventHandlerTest.java
@@ -33,21 +33,21 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
 @RunWith(MockitoJUnitRunner.class)
-public class DispatchEventTest {
+public class ForwardedEventHandlerTest {
 
   @Rule public ExpectedException exception = ExpectedException.none();
   @Mock private EventDispatcher dispatcherMock;
-  private DispatchEvent dispatchEvent;
+  private ForwardedEventHandler forwardedEventHandler;
 
   @Before
   public void setUp() throws Exception {
-    dispatchEvent = new DispatchEvent(dispatcherMock);
+    forwardedEventHandler = new ForwardedEventHandler(dispatcherMock);
   }
 
   @Test
   public void testSuccessfulDispatching() throws Exception {
     Event event = new ProjectCreatedEvent();
-    dispatchEvent.dispatch(event);
+    forwardedEventHandler.dispatch(event);
     verify(dispatcherMock).postEvent(event);
   }
 
@@ -66,7 +66,7 @@ public class DispatchEventTest {
         .postEvent(event);
 
     assertThat(Context.isForwardedEvent()).isFalse();
-    dispatchEvent.dispatch(event);
+    forwardedEventHandler.dispatch(event);
     assertThat(Context.isForwardedEvent()).isFalse();
 
     verify(dispatcherMock).postEvent(event);
@@ -86,7 +86,7 @@ public class DispatchEventTest {
 
     assertThat(Context.isForwardedEvent()).isFalse();
     try {
-      dispatchEvent.dispatch(event);
+      forwardedEventHandler.dispatch(event);
       fail("should have throw an OrmException");
     } catch (OrmException e) {
       assertThat(e.getMessage()).isEqualTo("someMessage");

GerritCodeReview/plugins_multi-site

Change-Id: I9841dee2d204b882b1e116900624a7329c79a968

src/test/java/com/ericsson/gerrit/plugins/highavailability/forwarder/ForwardedEventHandlerTest.java
@@ -33,21 +33,21 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
 @RunWith(MockitoJUnitRunner.class)
-public class DispatchEventTest {
+public class ForwardedEventHandlerTest {
 
   @Rule public ExpectedException exception = ExpectedException.none();
   @Mock private EventDispatcher dispatcherMock;
-  private DispatchEvent dispatchEvent;
+  private ForwardedEventHandler handler;
 
   @Before
   public void setUp() throws Exception {
-    dispatchEvent = new DispatchEvent(dispatcherMock);
+    handler = new ForwardedEventHandler(dispatcherMock);
   }
 
   @Test
   public void testSuccessfulDispatching() throws Exception {
     Event event = new ProjectCreatedEvent();
-    dispatchEvent.dispatch(event);
+    handler.dispatch(event);
     verify(dispatcherMock).postEvent(event);
   }
 
@@ -66,7 +66,7 @@ public class DispatchEventTest {
         .postEvent(event);
 
     assertThat(Context.isForwardedEvent()).isFalse();
-    dispatchEvent.dispatch(event);
+    handler.dispatch(event);
     assertThat(Context.isForwardedEvent()).isFalse();
 
     verify(dispatcherMock).postEvent(event);
@@ -86,7 +86,7 @@ public class DispatchEventTest {
 
     assertThat(Context.isForwardedEvent()).isFalse();
     try {
-      dispatchEvent.dispatch(event);
+      handler.dispatch(event);
       fail("should have throw an OrmException");
     } catch (OrmException e) {
       assertThat(e.getMessage()).isEqualTo("someMessage");