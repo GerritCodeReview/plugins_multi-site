a5f4363ddb3ac46d3bacbda89427d6260f7f2513
Fabio Ponciroli <ponch78@gmail.com>
2019-03-01 20:53:46 +0100

    Added multi-site plugin to dockerised environment
    Current components setup:
        - 2 gerrit instances
        - replication plugin
        - multi-site plugin
    
    Still need to add and configure:
        - haproxy
        - healthcheck
    
    Feature: Issue 10551
    
    Change-Id: I6d7508ed316d5029d9bbd5ff4e07ea23d6812928

--- dockerised_local_env/gerrit-2/docker-entrypoint.sh
+++ dockerised_local_env/gerrit-2/docker-entrypoint.sh
@@ -9,8 +9,11 @@ if [[ $INIT == 1 ]]; then
   echo "Remove git repos created during init phase"
   rm -fr /var/gerrit/git/*
 
-  echo "Waiting for initial replication"
+  echo "Waiting for gerrit1 server to become available."
   sleep 120
+  ssh -p 29418 admin@gerrit-1 replication start
+  echo "Waiting for replication to complete."
+  sleep 30
 fi
 
 java -jar /var/gerrit/bin/gerrit.war daemon
