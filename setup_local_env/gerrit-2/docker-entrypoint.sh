b190c5d1ea0299dfd22d27493aea2ebc7d618495
Fabio Ponciroli <ponch78@gmail.com>
2019-03-01 17:34:53 +0100

    Create Dockerised test environment
    Current components setup:
    - 2 gerrit instances
    - replication plugin
    
    Still need to add and configure:
    - haproxy
    - multi-site plugin
    - healthcheck
    
    Change-Id: Ia8068740e1b33213421787c1c426e19d9d3961f8

---
+++ setup_local_env/gerrit-2/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+
+java -Xmx100g -jar /var/gerrit/bin/gerrit.war init -d /var/gerrit --batch --dev --no-auto-start
+#
+echo "Gerrit configuration:"
+cat /var/gerrit/etc/gerrit.config
+echo "Replication plugin configuration:"
+cat /var/gerrit/etc/replication.config
+
+echo "Remove git repos created during init phase"
+rm -fr /var/gerrit/git/*
+
+echo "Starting git daemon"
+/usr/local/bin/git-daemon.sh &
+
+echo "Waiting for initial replication"
+sleep 120
+
+java -Xmx100g -jar /var/gerrit/bin/gerrit.war daemon
