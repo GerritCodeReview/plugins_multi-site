1a7cc0ea49a783fed89568677b8e78e6831939ad
Stefano Galarraga <galarragas@gmail.com>
2019-03-08 18:34:04 +0000

    Add embrional support for split brain detection
    Adds a RefOperationValidationListener to intercept and block operations
    if they might cause a split brain, i.e. if the oldId of the refs
    they are based on is not the currently most recent in the cluster.
    The current code doesn't provide a real implementation of the
    centralised DB but just an in memory implementation and a no-op one.
    The in memory implementation is probably useless but shows the mechanics
    of the centralised one.
    
    The Integration Tests are currently just validating that the new code doesn't
    introduce any regression.
    
    Configuration for the in-memory module has not been exposed.
    
    Feature: Issue 10554
    Change-Id: I8351ddb386a9528721c1c9670e3304b40f88a67c

---
+++ src/test/java/com/googlesource/gerrit/plugins/multisite/validation/ValidationIT.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite.validation;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.LogThreshold;
+import com.google.gerrit.acceptance.NoHttpd;
+import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.inject.AbstractModule;
+import org.junit.Test;
+
+@NoHttpd
+@LogThreshold(level = "INFO")
+@TestPlugin(
+    name = "multi-site",
+    sysModule = "com.googlesource.gerrit.plugins.multisite.validation.ValidationIT$Module")
+public class ValidationIT extends LightweightPluginDaemonTest {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  public static class Module extends AbstractModule {
+    @Override
+    protected void configure() {
+      install(new ValidationModule());
+    }
+  }
+
+  @Test
+  public void inSyncChangeValidatorShouldAcceptNewChange() throws Exception {
+    final PushOneCommit.Result change = createChange("refs/for/master");
+
+    change.assertOkStatus();
+  }
+}
