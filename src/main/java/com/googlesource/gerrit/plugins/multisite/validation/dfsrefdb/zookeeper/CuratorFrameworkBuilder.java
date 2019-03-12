ad69248445d2f34bfdfd0b346d1dcb4112751c21
Stefano Galarraga <galarragas@gmail.com>
2019-03-12 17:25:22 +0000

    Implement Zookeeper backend for consistency check
    Initial Zookeeper-based SharedDfsStorage implementation based on
    the original implementation of Dave Borowitz of zookeeper-refdb
    (https://gerrit.googlesource.com/plugins/zookeeper-refdb/)
    
    Feature: Issue 10554
    Change-Id: Ica87fe2c8a2c4aca5f9331bc0f7dcfb834d94727

---
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/dfsrefdb/zookeeper/CuratorFrameworkBuilder.java
@@ -0,0 +1,39 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite.validation.dfsrefdb.zookeeper;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+// TODO: REMOVE
+//public class CuratorFrameworkBuilder {
+//  private ZkConfig config = null;
+//
+//  public CuratorFrameworkBuilder config(ZkConfig config) {
+//    this.config = config;
+//    return this;
+//  }
+//
+//  public CuratorFramework build() {
+//    return CuratorFrameworkFactory.builder()
+//        .connectString(config.getConnectString())
+//        .sessionTimeoutMs(config.getSessionTimeoutMs())
+//        .connectionTimeoutMs(config.getConnectionTimeoutMs())
+//        .retryPolicy(new ExponentialBackoffRetry(40, 20))
+//        .namespace(config.getZookeeperRoot())
+//        .build();
+//  }
+//}
