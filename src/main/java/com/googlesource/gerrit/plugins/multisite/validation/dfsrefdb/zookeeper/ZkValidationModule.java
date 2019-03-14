73f9c05ad84807918605f398d996ed7b1f5ee929
Stefano Galarraga <galarragas@gmail.com>
2019-03-14 16:05:23 +0000

    Simplify the Zookeeper-based SharedDB code
    Store the ObjectId instead of a complex object, massively
    reduce the amount of code needed.
    
    Feature: Issue 10554
    Change-Id: I050d85584e0970f18e09c3107ca4e392affb6027

--- src/main/java/com/googlesource/gerrit/plugins/multisite/validation/dfsrefdb/zookeeper/ZkValidationModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/dfsrefdb/zookeeper/ZkValidationModule.java
@@ -12,26 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.googlesource.gerrit.plugins.multisite.validation;
+package com.googlesource.gerrit.plugins.multisite.validation.dfsrefdb.zookeeper;
 
-import com.google.gerrit.extensions.registration.DynamicSet;
-import com.google.gerrit.server.git.validators.RefOperationValidationListener;
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 import com.googlesource.gerrit.plugins.multisite.Configuration;
-import com.googlesource.gerrit.plugins.multisite.validation.dfsrefdb.zookeeper.ZkValidationModule;
+import com.googlesource.gerrit.plugins.multisite.validation.dfsrefdb.SharedRefDatabase;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
 
-public class ValidationModule extends AbstractModule {
+public class ZkValidationModule extends AbstractModule {
 
   private Configuration cfg;
 
-  public ValidationModule(Configuration cfg) {
+  public ZkValidationModule(Configuration cfg) {
     this.cfg = cfg;
   }
 
   @Override
   protected void configure() {
-    DynamicSet.bind(binder(), RefOperationValidationListener.class).to(InSyncChangeValidator.class);
-
-    install(new ZkValidationModule(cfg));
+    bind(SharedRefDatabase.class).to(ZkSharedRefDatabase.class);
+    bind(CuratorFramework.class).toInstance(cfg.getSplitBrain().getZookeeper().buildCurator());
+    bind(RetryPolicy.class)
+        .annotatedWith(Names.named("ZkLockRetryPolicy"))
+        .toInstance(cfg.getSplitBrain().getZookeeper().buildCasRetryPolicy());
   }
 }
