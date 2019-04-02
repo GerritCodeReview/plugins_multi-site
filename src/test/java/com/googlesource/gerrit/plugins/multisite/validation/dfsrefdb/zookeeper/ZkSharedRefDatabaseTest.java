888920148a51f8a4f8c3520c03c0b8acece456c1
Luca Milanesio <luca.milanesio@gmail.com>
2019-04-02 13:35:05 +0100

    Fix NPE when creating new refs
    When a new ref gets created, it translates to a compare and
    put of a null object id value, which translates to a 160-bytes
    array of zeros.
    
    Change-Id: Id725d4fca36efb3cb184582ec7b194c7ed9e25b8

--- src/test/java/com/googlesource/gerrit/plugins/multisite/validation/dfsrefdb/zookeeper/ZkSharedRefDatabaseTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/multisite/validation/dfsrefdb/zookeeper/ZkSharedRefDatabaseTest.java
@@ -82,6 +82,17 @@ public class ZkSharedRefDatabaseTest implements RefFixture {
   }
 
   @Test
+  public void shouldCompareAndPutWithNullOldRefSuccessfully() throws Exception {
+    Ref oldRef = refOf(null);
+    Ref newRef = refOf(AN_OBJECT_ID_2);
+    String projectName = A_TEST_PROJECT_NAME;
+
+    zookeeperContainer.createRefInZk(projectName, oldRef);
+
+    assertThat(zkSharedRefDatabase.compareAndPut(projectName, oldRef, newRef)).isTrue();
+  }
+
+  @Test
   public void compareAndPutShouldFailIfTheObjectionHasNotTheExpectedValue() throws Exception {
     String projectName = A_TEST_PROJECT_NAME;
 
