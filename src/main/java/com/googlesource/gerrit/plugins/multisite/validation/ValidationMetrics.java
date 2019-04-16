1a0f9e85bd24e398bd071293cc32de1431a54675
Stefano Galarraga <galarragas@gmail.com>
2019-04-16 09:41:13 +0100

    Add metric to count number of detected split-brain ref-updates
    Adds a new metric to count number of detected ref-update alteration blocked by
    split-brain validation
    
    Feature: Issue 10706
    Change-Id: I82d3fca9aa11a87b10ca9d3da8b6ffea075bc11c

---
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/ValidationMetrics.java
@@ -0,0 +1,29 @@
+package com.googlesource.gerrit.plugins.multisite.validation;
+
+import com.google.gerrit.metrics.Counter1;
+import com.google.gerrit.metrics.Description;
+import com.google.gerrit.metrics.Field;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class ValidationMetrics {
+  private static final String REF_UPDATES = "ref-updates";
+
+  private final Counter1<String> splitBrain;
+
+  @Inject
+  public ValidationMetrics(MetricMaker metricMaker) {
+    this.splitBrain =
+        metricMaker.newCounter(
+            "multi_site/validation/split_brain/",
+            new Description("Rate of REST API error responses").setRate().setUnit("errors"),
+            Field.ofString(
+                REF_UPDATES, "Ref-update operations detected as leading to split-brain"));
+  }
+
+  public void incrementSplitBrainRefUpdates() {
+    splitBrain.increment(REF_UPDATES);
+  }
+}
