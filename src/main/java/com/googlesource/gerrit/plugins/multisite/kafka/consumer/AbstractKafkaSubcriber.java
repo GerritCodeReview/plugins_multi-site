d93665cb081ee2f26fbceff1ac85ce95850075b2
David Ostrovsky <david@ostrovsky.org>
2019-05-25 18:38:31 +0200

    AbstractKafkaSubcriber: Fix DefaultCharset warning
    When all error prone warnings are enabled the DefaultCharset bug
    pattern is reported:
    
    AbstractKafkaSubcriber.java:136: error: [DefaultCharset] Implicit use \
    of the platform default charset, which can result in differing \
    behaviour between JVM executions or incorrect behavior if the encoding \
    of the data source doesn't match expectations.
       "Malformed event '%s': [Exception: %s]", new String(consumerRecord.value()));
                                                ^
        (see https://errorprone.info/bugpattern/DefaultCharset)
    
    Change-Id: I34ce395eacf779e60708b19f127b77284a6e7c5b

--- src/main/java/com/googlesource/gerrit/plugins/multisite/kafka/consumer/AbstractKafkaSubcriber.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/kafka/consumer/AbstractKafkaSubcriber.java
@@ -14,6 +14,8 @@
 
 package com.googlesource.gerrit.plugins.multisite.kafka.consumer;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.server.permissions.PermissionBackendException;
@@ -133,7 +135,7 @@ public abstract class AbstractKafkaSubcriber implements Runnable {
       }
     } catch (Exception e) {
       logger.atSevere().withCause(e).log(
-          "Malformed event '%s': [Exception: %s]", new String(consumerRecord.value()));
+          "Malformed event '%s': [Exception: %s]", new String(consumerRecord.value(), UTF_8));
     }
   }
 
