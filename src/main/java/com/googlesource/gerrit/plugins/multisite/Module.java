39664af648098c77042acd94af8f03511390bf7f
David Ostrovsky <david@ostrovsky.org>
2019-05-25 17:41:44 +0200

    Module: Fix DefaultCharset warning
    When all error prone warnings are enabled the DefaultCharset bug
    pattern is reported:
    
    Module.java:115: error: [DefaultCharset] Implicit use of the platform \
    default charset, which can result in differing behaviour between JVM \
    executions or incorrect behavior if the encoding of the data source \
    doesn't match expectations.
        try (BufferedReader br = new BufferedReader(new FileReader(serverIdFile))) {
                                                      ^
        (see https://errorprone.info/bugpattern/DefaultCharset)
    
    Change-Id: I7a4ef6bf920ef09e6d53b797b0adfab14bc961aa

--- src/main/java/com/googlesource/gerrit/plugins/multisite/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/Module.java
@@ -14,6 +14,8 @@
 
 package com.googlesource.gerrit.plugins.multisite;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.google.gerrit.lifecycle.LifecycleModule;
 import com.google.gerrit.server.config.SitePaths;
 import com.google.inject.CreationException;
@@ -30,7 +32,6 @@ import com.googlesource.gerrit.plugins.multisite.kafka.consumer.KafkaConsumerMod
 import com.googlesource.gerrit.plugins.multisite.kafka.router.ForwardedEventRouterModule;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -112,7 +113,7 @@ public class Module extends LifecycleModule {
 
   private UUID tryToLoadSavedInstanceId(String serverIdFile) {
     if (Files.exists(Paths.get(serverIdFile))) {
-      try (BufferedReader br = new BufferedReader(new FileReader(serverIdFile))) {
+      try (BufferedReader br = Files.newBufferedReader(Paths.get(serverIdFile), UTF_8)) {
         return UUID.fromString(br.readLine());
       } catch (IOException e) {
         log.warn(
