0b6604ae2c7761c80154ef15f79c3cb4e3c94d11
Tiago Palma <tiago.f.palma@gmail.com>
2019-06-11 18:27:12 +0200

    Introduce a NoOp Shared-Dir implementation
    NoOp implementation of the SharedRefDatabase interface.
    
    Feature: Issue 10830
    Change-Id: I694cc27a7954ee4d443c3e98a0e67969f542c66f

---
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/dfsrefdb/SharedRefDatabaseNoOp.java
@@ -0,0 +1,70 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite.validation.dfsrefdb;
+
+import com.google.common.flogger.FluentLogger;
+import java.io.IOException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+
+public class SharedRefDatabaseNoOp implements SharedRefDatabase {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  @Override
+  public boolean compareAndCreate(String project, Ref newRef) throws IOException {
+    logger.atFine().log(
+        "SharedRefDb NoOp - compareAndCreate(%s,%s). Will return true", project, newRef);
+    return true;
+  }
+
+  @Override
+  public boolean isUpToDate(String project, Ref ref) throws SharedLockException {
+    logger.atFine().log("SharedRefDb NoOp - isUpToDate(%s,%s). Will return true", project, ref);
+    return true;
+  }
+
+  @Override
+  public boolean compareAndPut(String project, Ref currRef, ObjectId newRefValue)
+      throws IOException {
+    logger.atFine().log(
+        "SharedRefDb NoOp - compareAndPut(%s, %s, %s). Will return true",
+        project, currRef, newRefValue);
+    return true;
+  }
+
+  @Override
+  public boolean compareAndRemove(String project, Ref oldRef) throws IOException {
+    logger.atFine().log(
+        "SharedRefDb NoOp - compareAndRemove(%s, %s). Will return true", project, oldRef);
+    return true;
+  }
+
+  @Override
+  public AutoCloseable lockRef(String project, String refName) throws SharedLockException {
+    logger.atFine().log("SharedRefDb NoOp - lockRef(%s, %s). Will return null", project, refName);
+    return null;
+  }
+
+  @Override
+  public boolean exists(String project, String refName) {
+    logger.atFine().log("SharedRefDb NoOp - exists(%s, %s). Will return true", project, refName);
+    return true;
+  }
+
+  @Override
+  public void removeProject(String project) throws IOException {
+    logger.atFine().log("SharedRefDb NoOp - removeProject(%s)", project);
+  }
+}
