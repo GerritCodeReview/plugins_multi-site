cefcc83bfad3cd8a6520f6145deb8cea2ec14005
Antonio Barone <syntonyze@gmail.com>
2019-07-16 19:02:40 +0100

    Introduce sharedRef database log file
    Log to file all mutations to the sharedRef database so that each
    interaction (creation, update and deletion) is recorded for analysis,
    triage and debugging of issues.
    
    Note:
    Because multi-site gets installed as libModule, rather than a plugin,
    PluginLogFile interface cannot be used, thus log appenders are defined
    in a new LibModuleLogFile.
    
    Feature: Issue 11166
    Change-Id: I940c48238d8c6b88a439bc8400692de87ece3400

---
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/Log4jSharedRefLogger.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite;
+
+import com.google.gerrit.server.util.SystemLog;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.apache.log4j.PatternLayout;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class Log4jSharedRefLogger extends LibModuleLogFile implements SharedRefLogger {
+  private static final String LOG_NAME = "sharedRefDB_log";
+  private final Logger sharedRefDBLog;
+
+  @Inject
+  public Log4jSharedRefLogger(SystemLog systemLog) {
+    super(systemLog, LOG_NAME, new PatternLayout("[%d{ISO8601}] [%t] %-5p : %m%n"));
+    sharedRefDBLog = LoggerFactory.getLogger(LOG_NAME);
+  }
+
+  @Override
+  public void log(String project, Ref currRef, ObjectId newRefValue) {
+    sharedRefDBLog.info(
+        "project:{}|ref:{}|oldId:{}|newId:{}",
+        project,
+        currRef.getName(),
+        currRef.getObjectId().getName(),
+        newRefValue.getName());
+  }
+
+  @Override
+  public void logDeletion(String project) {
+    sharedRefDBLog.info("project:{}|DELETED", project);
+  }
+}
