0479289ce04c472e31a9ce8479e0f374abcb8bb2
Marcin Czech <maczech@gmail.com>
2019-10-10 22:23:46 +0200

    Remove kafka related code from multi-site
    Kafka-events plugin provides kafka implementation of BrokerApi.
    Code related to kafka is no longer needed in multi-site. By
    default multi-site is binding NoOp implementation which can be replaced with
    other plugins for example by kafka-events plugin.
    
    Feature: Issue 11599
    Change-Id: Ia259939f2c8bea90683bd9a936ed852ba1d08f0e

--- src/main/java/com/googlesource/gerrit/plugins/multisite/consumer/SubscriberModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/consumer/SubscriberModule.java
@@ -16,22 +16,21 @@ package com.googlesource.gerrit.plugins.multisite.consumer;
 
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.lifecycle.LifecycleModule;
-import com.googlesource.gerrit.plugins.multisite.forwarder.events.EventTopic;
 import com.googlesource.gerrit.plugins.multisite.forwarder.events.MultiSiteEvent;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class SubscriberModule extends LifecycleModule {
 
   @Override
   protected void configure() {
     MultiSiteEvent.registerEventTypes();
-    bind(ExecutorService.class)
-        .annotatedWith(ConsumerExecutor.class)
-        .toInstance(Executors.newFixedThreadPool(EventTopic.values().length));
-    listener().to(MultiSiteConsumerRunner.class);
 
     DynamicSet.setOf(binder(), AbstractSubcriber.class);
     DynamicSet.setOf(binder(), DroppedEventListener.class);
+    listener().to(MultiSiteConsumerRunner.class);
+
+    DynamicSet.bind(binder(), AbstractSubcriber.class).to(IndexEventSubscriber.class);
+    DynamicSet.bind(binder(), AbstractSubcriber.class).to(StreamEventSubscriber.class);
+    DynamicSet.bind(binder(), AbstractSubcriber.class).to(CacheEvictionEventSubscriber.class);
+    DynamicSet.bind(binder(), AbstractSubcriber.class).to(ProjectUpdateEventSubscriber.class);
   }
 }
