802c653c14977180a1447a18c1b26408d739fb61
Luca Milanesio <luca.milanesio@gmail.com>
2020-02-22 01:43:40 +0000

    Update subscriber metrics at every replication stage
    Keep on updating the subscriber's replication lag metrics
    at every stage of the replication.
    
    This assures that even in case of a replication failure, the metrics
    keep on representing the current status of the remote nodes.
    
    Change-Id: Ic23394717fd64be7a99a47258f087b952bb06be2

--- src/main/java/com/googlesource/gerrit/plugins/multisite/consumer/SubscriberMetrics.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/consumer/SubscriberMetrics.java
@@ -20,12 +20,15 @@ import com.google.gerrit.metrics.Counter1;
 import com.google.gerrit.metrics.Description;
 import com.google.gerrit.metrics.Field;
 import com.google.gerrit.metrics.MetricMaker;
+import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.events.Event;
+import com.google.gerrit.server.events.RefUpdatedEvent;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.multisite.validation.ProjectVersionRefUpdate;
 import com.googlesource.gerrit.plugins.replication.RefReplicatedEvent;
 import com.googlesource.gerrit.plugins.replication.RefReplicationDoneEvent;
+import com.googlesource.gerrit.plugins.replication.ReplicationScheduledEvent;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -92,21 +95,35 @@ public class SubscriberMetrics {
   public void updateReplicationStatusMetrics(EventMessage eventMessage) {
     Event event = eventMessage.getEvent();
     if (event instanceof RefReplicationDoneEvent) {
-      RefReplicationDoneEvent refReplicationDoneEvent = (RefReplicationDoneEvent) event;
-      String projectName = refReplicationDoneEvent.getProjectNameKey().get();
-      Optional<Long> remoteVersion = projectVersionRefUpdate.getProjectRemoteVersion(projectName);
-      Optional<Long> localVersion = projectVersionRefUpdate.getProjectLocalVersion(projectName);
-      if (remoteVersion.isPresent() && localVersion.isPresent()) {
-        long lag = remoteVersion.get() - localVersion.get();
-        logger.atFine().log(
-            "Published replication lag metric for project '%s' of %d sec(s) [local-ref=%d global-ref=%d]",
-            projectName, lag, localVersion.get(), remoteVersion.get());
-        replicationStatusPerProject.put(projectName, lag);
-      } else {
-        logger.atFine().log(
-            "Did not publish replication lag metric for %s because the %s version is not defined",
-            projectName, localVersion.isPresent() ? "remote" : "local");
-      }
+      RefReplicationDoneEvent replicationDone = (RefReplicationDoneEvent) event;
+      updateReplicationLagMetrics(
+          replicationDone.getProjectNameKey(), replicationDone.getRefName());
+    } else if (event instanceof RefReplicatedEvent) {
+      RefReplicatedEvent replicated = (RefReplicatedEvent) event;
+      updateReplicationLagMetrics(replicated.getProjectNameKey(), replicated.getRefName());
+    } else if (event instanceof ReplicationScheduledEvent) {
+      ReplicationScheduledEvent updated = (ReplicationScheduledEvent) event;
+      updateReplicationLagMetrics(updated.getProjectNameKey(), updated.getRefName());
+    } else if (event instanceof RefUpdatedEvent) {
+      RefUpdatedEvent updated = (RefUpdatedEvent) event;
+      updateReplicationLagMetrics(updated.getProjectNameKey(), updated.getRefName());
+    }
+  }
+
+  private void updateReplicationLagMetrics(Project.NameKey projectName, String ref) {
+    Optional<Long> remoteVersion =
+        projectVersionRefUpdate.getProjectRemoteVersion(projectName.get());
+    Optional<Long> localVersion = projectVersionRefUpdate.getProjectLocalVersion(projectName.get());
+    if (remoteVersion.isPresent() && localVersion.isPresent()) {
+      long lag = remoteVersion.get() - localVersion.get();
+      logger.atFine().log(
+          "Published replication lag metric for project '%s' of %d sec(s) [local-ref=%d global-ref=%d]",
+          projectName, lag, localVersion.get(), remoteVersion.get());
+      replicationStatusPerProject.put(projectName.get(), lag);
+    } else {
+      logger.atFine().log(
+          "Did not publish replication lag metric for %s because the %s version is not defined",
+          projectName, localVersion.isPresent() ? "remote" : "local");
     }
   }
 }
