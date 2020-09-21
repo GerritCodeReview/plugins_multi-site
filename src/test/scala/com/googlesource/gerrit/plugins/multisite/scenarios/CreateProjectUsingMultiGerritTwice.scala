5b68f3c447fafc0d376069a64985b14813cbbcad
Marco Miller <marco.miller@ericsson.com>
2020-09-21 16:27:05 -0400

    e2e-tests: Remove unnecessary type specifications
    As these do not lend warnings in IntelliJ once removed.
    
    Change-Id: I1979352e0d31da44c1bb72e95491154176f7335c

--- src/test/scala/com/googlesource/gerrit/plugins/multisite/scenarios/CreateProjectUsingMultiGerritTwice.scala
+++ src/test/scala/com/googlesource/gerrit/plugins/multisite/scenarios/CreateProjectUsingMultiGerritTwice.scala
@@ -20,7 +20,7 @@ import io.gatling.core.Predef.{atOnceUsers, _}
 import scala.concurrent.duration._
 
 class CreateProjectUsingMultiGerritTwice extends GitSimulation {
-  private val default: String = name
+  private val default = name
 
   private val createProject = new CreateProjectUsingMultiGerrit(default)
   private val deleteProject = new DeleteProjectUsingMultiGerrit(default)
