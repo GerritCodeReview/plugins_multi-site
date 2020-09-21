e3824558693255cc4fe8fefdf919ca70ecdcb11f
Marco Miller <marco.miller@ericsson.com>
2020-09-21 17:31:00 -0400

    e2e-tests: Rename 'unique' after GerritSimulation core change
    Change-Id: Id69f79fdb2253a6669accffad38ee3826f04edb0

--- src/test/scala/com/googlesource/gerrit/plugins/multisite/scenarios/CloneUsingMultiGerrit1.scala
+++ src/test/scala/com/googlesource/gerrit/plugins/multisite/scenarios/CloneUsingMultiGerrit1.scala
@@ -35,7 +35,7 @@ class CloneUsingMultiGerrit1 extends GitSimulation {
     replaceKeyWith("_project", default, next)
   }
 
-  val test: ScenarioBuilder = scenario(unique)
+  val test: ScenarioBuilder = scenario(uniqueName)
     .feed(data)
     .exec(gitRequest)
 
