e65fc1622063897ef4b3714440de27c8d925a2bc
Luca Milanesio <luca.milanesio@gmail.com>
2021-03-19 01:19:22 +0000

    Test cache invalidation behaviour inside a Gerrit daemon
    All the existing tests for cache forwarding were not plugging
    the real Gerrit caches. A change happened in the project cache
    in stable-3.3 broke the cache invalidation without breaking
    the tests.
    
    Perform an effective E2E cache invalidation test from
    a plugin integration test environment, so that if any of
    the cache keys are changing, the test would allow to
    identify the breakage and the associated cache involved.
    
    The first cache included in the E2E test is 'projects'.
    Other caches are going to be covered in the follow-up
    changes.
    
    Bug: Issue 14255
    Change-Id: I94db246f6310f898ee673dc1a9bad31ffedeef83

---
+++ src/test/java/com/googlesource/gerrit/plugins/multisite/forwarder/ForwardedCacheEvictionHandlerIT.java
@@ -0,0 +1,114 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite.forwarder;
+
+import static com.google.gerrit.acceptance.WaitUtil.waitUntil;
+
+import com.gerritforge.gerrit.globalrefdb.validation.SharedRefDbConfiguration;
+import com.google.common.cache.RemovalNotification;
+import com.google.common.collect.Sets;
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.cache.CacheRemovalListener;
+import com.google.gerrit.server.events.EventGson;
+import com.google.gerrit.server.project.ProjectCacheImpl;
+import com.google.gson.Gson;
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.googlesource.gerrit.plugins.multisite.cache.CacheModule;
+import com.googlesource.gerrit.plugins.multisite.forwarder.events.CacheEvictionEvent;
+import com.googlesource.gerrit.plugins.multisite.forwarder.router.CacheEvictionEventRouter;
+import com.googlesource.gerrit.plugins.multisite.forwarder.router.RouterModule;
+import com.googlesource.gerrit.plugins.multisite.index.IndexModule;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Test;
+
+@TestPlugin(
+    name = "multi-site",
+    sysModule =
+        "com.googlesource.gerrit.plugins.multisite.forwarder.ForwardedCacheEvictionHandlerIT$TestModule")
+public class ForwardedCacheEvictionHandlerIT extends LightweightPluginDaemonTest {
+
+  private static final Duration CACHE_EVICTION_TIMEOUT = Duration.ofSeconds(1);
+
+  public static class TestModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+      install(new ForwarderModule());
+      install(new CacheModule());
+      install(new RouterModule());
+      install(new IndexModule());
+      SharedRefDbConfiguration sharedRefDbConfig =
+          new SharedRefDbConfiguration(new Config(), "multi-site");
+      bind(SharedRefDbConfiguration.class).toInstance(sharedRefDbConfig);
+      DynamicSet.bind(binder(), CacheRemovalListener.class).to(CacheEvictionTest.class);
+    }
+  }
+
+  public static class CacheEvictionTest<K, V> implements CacheRemovalListener<K, V> {
+    private static Map<String, Set<Object>> cacheEvictions = new ConcurrentHashMap<>();
+
+    public static void clear() {
+      cacheEvictions.clear();
+    }
+
+    public static Set<Object> cacheEvictions(String cacheName) {
+      return cacheEvictions.getOrDefault(cacheName, Collections.emptySet());
+    }
+
+    @Override
+    public void onRemoval(
+        String pluginName, String cacheName, RemovalNotification<K, V> notification) {
+      cacheEvictions.compute(
+          cacheName,
+          (k, v) -> {
+            if (v == null) {
+              return Sets.newHashSet(notification.getKey());
+            }
+            v.add(notification.getKey());
+            return v;
+          });
+    }
+  }
+
+  private CacheEvictionEventRouter objectUnderTest;
+  private Gson gson;
+
+  @Override
+  public void setUpTestPlugin() throws Exception {
+    super.setUpTestPlugin();
+    Injector injector = plugin.getSysInjector();
+    objectUnderTest = injector.getInstance(CacheEvictionEventRouter.class);
+    gson = injector.getInstance(Key.get(Gson.class, EventGson.class));
+  }
+
+  @Test
+  public void shouldEvictProjectCache() throws Exception {
+    CacheEvictionTest.clear();
+    objectUnderTest.route(
+        new CacheEvictionEvent(ProjectCacheImpl.CACHE_NAME, gson.toJson(project)));
+    waitUntil(
+        () -> CacheEvictionTest.cacheEvictions(ProjectCacheImpl.CACHE_NAME).contains(project),
+        CACHE_EVICTION_TIMEOUT);
+  }
+}
