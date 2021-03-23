f0e623957b7a433992900e739ecbbdd005f60cd1
Antonio Barone <syntonyze@gmail.com>
2021-03-23 21:13:38 +0100

    Wait for groups replication before attempting indexing
    Do not immediately attempt to (re)index a group before the related has
    landed in git, which might take some time depending on replication
    delays and settings.
    
    Similarly to the ForwardedIndexChangeHandler, the
    ForwardedGroupChangeHandler will attempt multiple reindexes up to
    `index.maxTries`, every `index.retryInterval` milliseconds.
    
    To be able to understand whether replication is completed and the local
    group ref is up-to-date, two main changes have been introduced:
    
    - A GroupChecker class, with the responsibility to lookup in All-Users
      for the status of a specified group
    - An extra field in the GroupIndexEvent class, to indicate which
      particular sha1 the ref group was updated to, so that GroupChecker
      could perform a specific lookup.
    
    Note that to maintain back-compatibility with previous
    GroupIndexEvent's, the additional field was made optional, so that old
    events can still be de-serialized and consumed by this version of the
    multi-site code.
    
    Bug: Issue 14161
    Change-Id: Ifc171a49a0c360ceb39538a6d133154fcd0050e9

---
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/index/GroupCheckerImpl.java
@@ -0,0 +1,110 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite.index;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.AccountGroup;
+import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.multisite.forwarder.events.GroupIndexEvent;
+import java.util.Optional;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+@Singleton
+public class GroupCheckerImpl implements GroupChecker {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private final GitRepositoryManager repoManager;
+  private final AllUsersName allUsers;
+
+  @Inject
+  GroupCheckerImpl(GitRepositoryManager repoManager, AllUsersName allUsers) {
+
+    this.repoManager = repoManager;
+    this.allUsers = allUsers;
+  }
+
+  @Override
+  public boolean isGroupUpToDate(Optional<GroupIndexEvent> groupIndexEvent) {
+    if (!groupIndexEvent.isPresent()) {
+      logger.atWarning().log("Group Index empty, considering this group up-to-date");
+      return true;
+    }
+    GroupIndexEvent event = groupIndexEvent.get();
+    AccountGroup.UUID groupUUID = AccountGroup.uuid(event.groupUUID);
+
+    ObjectId groupHead = getGroupHead(event.groupUUID);
+
+    if (ObjectId.zeroId().equals(groupHead)) {
+      logger.atWarning().log("Group '%s' does not exist in All-Users", groupUUID.toString());
+      return false;
+    }
+
+    if (event.sha1 == null) {
+      logger.atWarning().log(
+          "Event for group '%s' does not contain sha1, consider group up-to-date for compatibility.",
+          groupUUID.toString());
+      return true;
+    }
+
+    if (groupHead.equals(event.sha1)) {
+      logger.atInfo().log(
+          "Head of group '%s' is up-to-date (sha1: %s)", groupUUID.toString(), event.sha1);
+      return true;
+    }
+
+    try (Repository repo = repoManager.openRepository(allUsers)) {
+      Ref groupRef = repo.exactRef(RefNames.refsGroups(groupUUID));
+
+      if (groupRef == null) {
+        logger.atWarning().log("Group '%s' does not exist in All-Users", groupUUID.toString());
+        return false;
+      }
+
+      try (RevWalk revWalk = new RevWalk(repo)) {
+        revWalk.parseCommit(event.sha1);
+        logger.atInfo().log(
+            "Group '%s' up-to-date: history contains sha1 '%s'", groupUUID.toString(), event.sha1);
+        return true;
+      }
+    } catch (MissingObjectException e) {
+      logger.atWarning().log(
+          "Group '%s' is not up-to-date: sha1 '%s' is still missing",
+          groupUUID.toString(), event.sha1);
+    } catch (Exception e) {
+      logger.atSevere().withCause(e).log(
+          "Could not check whether Group '%s' is up-to-date", groupUUID.toString());
+    }
+    return false;
+  }
+
+  @Override
+  public ObjectId getGroupHead(String groupUUID) {
+    try (Repository repo = repoManager.openRepository(allUsers)) {
+      return Optional.ofNullable(repo.exactRef(RefNames.refsGroups(AccountGroup.uuid(groupUUID))))
+          .map(Ref::getObjectId)
+          .orElse(ObjectId.zeroId());
+    } catch (Exception e) {
+      logger.atSevere().withCause(e).log("Fatal: could not get head of group %s.", groupUUID);
+      return ObjectId.zeroId();
+    }
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/multisite/index/GroupCheckerImplTest.java
@@ -0,0 +1,111 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite.index;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.entities.AccountGroup;
+import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.config.AllUsersNameProvider;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.googlesource.gerrit.plugins.multisite.forwarder.events.GroupIndexEvent;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectIdRef;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefDatabase;
+import org.eclipse.jgit.lib.Repository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GroupCheckerImplTest {
+  ObjectId AN_OBJECT_ID = ObjectId.fromString("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  ObjectId ANOTHER_OBJECT_ID = ObjectId.fromString("aabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+
+  GroupCheckerImpl objectUnderTest;
+  @Mock private GitRepositoryManager repoManagerMock;
+  @Mock private RefDatabase refDatabaseMock;
+  @Mock private Repository repoMock;
+
+  @Before
+  public void setUp() throws Exception {
+    AllUsersName allUsers = new AllUsersName(AllUsersNameProvider.DEFAULT);
+    doReturn(repoMock).when(repoManagerMock).openRepository(allUsers);
+    doReturn(refDatabaseMock).when(repoMock).getRefDatabase();
+    objectUnderTest = new GroupCheckerImpl(repoManagerMock, allUsers);
+  }
+
+  @Test
+  public void isGroupUpToDate_shouldReturnTrueWhenEventIsEmpty() {
+    assertThat(objectUnderTest.isGroupUpToDate(Optional.empty())).isTrue();
+  }
+
+  @Test
+  public void isGroupUpToDate_shouldReturnFalseWhenGroupDoesNotExist() {
+    assertThat(objectUnderTest.isGroupUpToDate(groupIndexEvent(UUID.randomUUID().toString(), null)))
+        .isFalse();
+  }
+
+  @Test
+  public void isGroupUpToDate_shouldReturnTrueWhenGroupExistsAndSha1IsNull() throws IOException {
+    UUID groupUUID = UUID.randomUUID();
+    setupExactRefInGroup(groupUUID, AN_OBJECT_ID);
+
+    assertThat(objectUnderTest.isGroupUpToDate(groupIndexEvent(groupUUID.toString(), null)))
+        .isTrue();
+  }
+
+  @Test
+  public void isGroupUpToDate_shouldReturnTrueWhenGroupExistsAndSha1MatchesTheGroupRef()
+      throws IOException {
+    UUID groupUUID = UUID.randomUUID();
+    setupExactRefInGroup(groupUUID, AN_OBJECT_ID);
+
+    assertThat(objectUnderTest.isGroupUpToDate(groupIndexEvent(groupUUID.toString(), AN_OBJECT_ID)))
+        .isTrue();
+  }
+
+  @Test
+  public void isGroupUpToDate_shouldReturnFalseWhenGroupExistsAndSha1DoesNotMatchTheGroupRef()
+      throws IOException {
+    UUID groupUUID = UUID.randomUUID();
+    setupExactRefInGroup(groupUUID, AN_OBJECT_ID);
+
+    assertThat(
+            objectUnderTest.isGroupUpToDate(
+                groupIndexEvent(groupUUID.toString(), ANOTHER_OBJECT_ID)))
+        .isFalse();
+  }
+
+  private Optional<GroupIndexEvent> groupIndexEvent(String uuid, @Nullable ObjectId sha1) {
+    return Optional.of(new GroupIndexEvent(uuid, sha1));
+  }
+
+  private void setupExactRefInGroup(UUID groupUUID, ObjectId objectId) throws IOException {
+    String groupRefName = RefNames.refsGroups(AccountGroup.uuid(groupUUID.toString()));
+    ObjectIdRef.Unpeeled aRef = new ObjectIdRef.Unpeeled(Ref.Storage.LOOSE, groupRefName, objectId);
+
+    doReturn(aRef).when(refDatabaseMock).exactRef(groupRefName);
+  }
+}
