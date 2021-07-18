2454c5cc3e150097035bd2429fbfd7917edc496c
Jacek Centkowski <geminica.programs@gmail.com>
2021-07-18 13:52:35 +0200

    Introduce local (docker) setup for e2e tests
    This is the initial commit that creates local (docker based) setup
    to run e2e tests. Tests are integrated with bazel and are part of
    test target:
    
      bazel test //plugins/multi-site/...
    
    One can also start them independently by calling:
    
      e2e-tests/test.sh
    
    For now it set all multi-site plugins as mandatory in Gerrit
    config and checks if everything gets started.
    
    See `e2e-tests/README.md` for requirements and details about the
    test setup and call:
    
      e2e-tests/test.sh --help
    
    for all configuration options.
    
    Bug: Issue 14509
    Change-Id: Ibb123a5e100d8abedb9035ba02ecb95acfab5ef8

---
+++ e2e-tests/scenarios.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SSH_OPTS='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+
+function call_gerrit {
+  SERVER=$1
+  shift
+  su gerrit -c "ssh -p 29418 ${SSH_OPTS} admin@${SERVER} gerrit \"$@\""
+}
+
+
+chown -R gerrit:gerrit /var/gerrit/.ssh
+
+call_gerrit gerrit1 version || exit 1;
+call_gerrit gerrit2 version || exit 1;
+echo "All tests were finished successfully."
