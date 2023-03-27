f5b07d5fc6a72c41f127af3ba228cffaf769f279
Luca Milanesio <luca.milanesio@gmail.com>
2023-03-27 23:35:34 +0100

    Implement pull-replication fetch filter for avoiding split-brain
    Define a concrete implementation of the ReplicationFetchFilter which
    allows to prevent split-brain by filtering out all refs that are
    already in-sync with the global-refdb.
    
    This is dependent from I19d586381 which introduces the optional
    interface ReplicationFetchFilter, implemented and bound in the
    multi-site's validation module.
    
    Also adapt the Jenkinsfile to allow the checkout of the pull-replication
    plugin as dependency.
    
    Depends-On: https://gerrit-review.googlesource.com/c/gerrit-ci-scripts/+/366460
    Bug: Issue 15637
    Change-Id: If60f5f5eaf700076e954004a4037cc70a8def2f1

---
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/MultisiteReplicationFetchFilter.java
@@ -0,0 +1,145 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite.validation;
+
+import com.gerritforge.gerrit.globalrefdb.GlobalRefDbLockException;
+import com.gerritforge.gerrit.globalrefdb.validation.SharedRefDatabaseWrapper;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.replication.pull.ReplicationFetchFilter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectIdRef;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefDatabase;
+import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class MultisiteReplicationFetchFilter implements ReplicationFetchFilter {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  public static final int MIN_WAIT_BEFORE_RELOAD_LOCAL_VERSION_MS = 1000;
+  public static final int RANDOM_WAIT_BEFORE_RELOAD_LOCAL_VERSION_MS = 1000;
+
+  static final String REPLICATION_LOG_NAME = "replication_log";
+  static final Logger repLog = LoggerFactory.getLogger(REPLICATION_LOG_NAME);
+
+  private final SharedRefDatabaseWrapper sharedRefDb;
+  private final GitRepositoryManager gitRepositoryManager;
+
+  @Inject
+  public MultisiteReplicationFetchFilter(
+      SharedRefDatabaseWrapper sharedRefDb, GitRepositoryManager gitRepositoryManager) {
+    this.sharedRefDb = sharedRefDb;
+    this.gitRepositoryManager = gitRepositoryManager;
+  }
+
+  @Override
+  public Set<String> filter(String projectName, Set<String> refs) {
+    try (Repository repository =
+        gitRepositoryManager.openRepository(Project.nameKey(projectName))) {
+      RefDatabase refDb = repository.getRefDatabase();
+      return refs.stream()
+          .filter(
+              ref -> {
+                Optional<ObjectId> localRefOid =
+                    getSha1IfUpToDateWithGlobalRefDb(repository, projectName, refDb, ref, true);
+                localRefOid.ifPresent(
+                    oid -> {
+                      repLog.info(
+                          "{}={} is already up-to-date with the shared-refdb and thus will NOT BE"
+                              + " fetched",
+                          ref,
+                          oid);
+                    });
+
+                return localRefOid.isEmpty();
+              })
+          .collect(Collectors.toSet());
+    } catch (IOException ioe) {
+      String message = String.format("Error while opening project: '%s'", projectName);
+      repLog.error(message);
+      logger.atSevere().withCause(ioe).log(message);
+      return Collections.emptySet();
+    }
+  }
+
+  private Optional<ObjectId> getSha1IfUpToDateWithGlobalRefDb(
+      Repository repository,
+      String projectName,
+      RefDatabase refDb,
+      String ref,
+      boolean retryWithRandomSleep) {
+    try {
+      Optional<ObjectId> localRefObjectId =
+          Optional.ofNullable(refDb.exactRef(ref))
+              .map(Ref::getObjectId)
+              .filter(
+                  oid ->
+                      sharedRefDb.isUpToDate(
+                          Project.nameKey(projectName),
+                          new ObjectIdRef.Unpeeled(Ref.Storage.NETWORK, ref, oid)));
+
+      if (!localRefObjectId.isPresent() && retryWithRandomSleep) {
+        randomSleepForMitigatingConditionWhereLocalRefHaveJustBeenChanged(
+            projectName, localRefObjectId, ref);
+        localRefObjectId =
+            getSha1IfUpToDateWithGlobalRefDb(repository, projectName, refDb, ref, false);
+      }
+
+      return localRefObjectId;
+    } catch (GlobalRefDbLockException gle) {
+      String message = String.format("%s is locked on shared-refdb", ref);
+      repLog.error(message);
+      logger.atSevere().withCause(gle).log(message);
+      return Optional.empty();
+    } catch (IOException ioe) {
+      String message =
+          String.format("Error while extracting ref '%s' for project '%s'", ref, projectName);
+      repLog.error(message);
+      logger.atSevere().withCause(ioe).log(message);
+      return Optional.empty();
+    }
+  }
+
+  private void randomSleepForMitigatingConditionWhereLocalRefHaveJustBeenChanged(
+      String projectName, Optional<ObjectId> refObjectId, String ref) {
+    int randomSleepTimeMsec =
+        MIN_WAIT_BEFORE_RELOAD_LOCAL_VERSION_MS
+            + new Random().nextInt(RANDOM_WAIT_BEFORE_RELOAD_LOCAL_VERSION_MS);
+    repLog.debug(
+        String.format(
+            "'%s' is not up-to-date for project '%s' [local='%s']. Reload local ref in '%d ms' and"
+                + " re-check",
+            ref, projectName, refObjectId, randomSleepTimeMsec));
+    try {
+      Thread.sleep(randomSleepTimeMsec);
+    } catch (InterruptedException ie) {
+      String message =
+          String.format("Error while waiting for next check for '%s', ref '%s'", projectName, ref);
+      repLog.error(message);
+      logger.atWarning().withCause(ie).log(message);
+    }
+  }
+}
