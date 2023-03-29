ef886bce4b0f6b55233af076f83d604c44d4346f
Luca Milanesio <luca.milanesio@gmail.com>
2023-03-29 23:57:03 +0100

    Rely on global-refdb singleton definition of SharedRefDatabaseWrapper
    The SharedRefDatabaseWrapper in global-refdb v3.4.8.3 is defined
    as singleton in its class annotation, therefore does not need anymore
    an explicit binding as singleton in the module.
    
    Also bump global-refdb to v3.4.8.3.
    
    Change-Id: I58253c4c3a7d195b151d6da9086ec13a83386e49

--- external_plugin_deps.bzl
+++ external_plugin_deps.bzl
@@ -3,8 +3,8 @@ load("//tools/bzl:maven_jar.bzl", "maven_jar")
 def external_plugin_deps():
     maven_jar(
         name = "global-refdb",
-        artifact = "com.gerritforge:global-refdb:3.4.8.2",
-        sha1 = "e389f948e2da0a34893d78cb4030a6ee157d66b7",
+        artifact = "com.gerritforge:global-refdb:3.4.8.3",
+        sha1 = "fde470b7a1e2eccd75e7ef4709db3ce117a05fb6",
     )
 
     maven_jar(
--- src/main/java/com/googlesource/gerrit/plugins/multisite/validation/ValidationModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/ValidationModule.java
@@ -18,7 +18,6 @@ import com.gerritforge.gerrit.globalrefdb.validation.BatchRefUpdateValidator;
 import com.gerritforge.gerrit.globalrefdb.validation.LockWrapper;
 import com.gerritforge.gerrit.globalrefdb.validation.Log4jSharedRefLogger;
 import com.gerritforge.gerrit.globalrefdb.validation.RefUpdateValidator;
-import com.gerritforge.gerrit.globalrefdb.validation.SharedRefDatabaseWrapper;
 import com.gerritforge.gerrit.globalrefdb.validation.SharedRefDbBatchRefUpdate;
 import com.gerritforge.gerrit.globalrefdb.validation.SharedRefDbGitRepositoryManager;
 import com.gerritforge.gerrit.globalrefdb.validation.SharedRefDbRefDatabase;
@@ -50,7 +49,6 @@ public class ValidationModule extends FactoryModule {
   protected void configure() {
     install(new ReplicationExtensionPointModule());
 
-    bind(SharedRefDatabaseWrapper.class).in(Scopes.SINGLETON);
     bind(SharedRefLogger.class).to(Log4jSharedRefLogger.class);
     factory(LockWrapper.Factory.class);
 
