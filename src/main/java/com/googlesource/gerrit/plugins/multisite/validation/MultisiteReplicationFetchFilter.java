e9ea26f16448a984e01f575cf5c40015c558345c
Marcin Czech <maczech@gmail.com>
2023-03-31 00:05:53 +0200

    Filter out non existing refs from fetch
    Exclude ref from fetch task when ref is not present in the local
    repository or has been removed in the global-refdb and therefore is
    set to zeros.
    
    P.S. The situation where the entry is not on the global-refdb is
    in doubt, because we don't really know if fetching the refs would
    succeed or not. The best approach is to fetch anyway and leave the
    Git protocol to decide. That allows the fetching from instances when
    the global-refdb has been initialised or for new repos that have
    been imported but are not yet fully tracked on the global-refdb.
    Furthermore, the global-refdb tracks only the mutable ref so
    we cannot assume that everything is tracked.
    
    Bug: Issue 16824
    Change-Id: I06a3bb73a805046ec5a8bc582586e7db4d213171

--- src/main/java/com/googlesource/gerrit/plugins/multisite/validation/MultisiteReplicationFetchFilter.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/MultisiteReplicationFetchFilter.java
@@ -20,6 +20,7 @@ import com.gerritforge.gerrit.globalrefdb.GlobalRefDbLockException;
 import com.gerritforge.gerrit.globalrefdb.validation.SharedRefDatabaseWrapper;
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.entities.Project;
+import com.google.gerrit.entities.Project.NameKey;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -37,6 +38,7 @@ import org.eclipse.jgit.lib.Repository;
 
 @Singleton
 public class MultisiteReplicationFetchFilter implements ReplicationFetchFilter {
+  private static final String ZERO_ID_NAME = ObjectId.zeroId().name();
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
   public static final int MIN_WAIT_BEFORE_RELOAD_LOCAL_VERSION_MS = 1000;
   public static final int RANDOM_WAIT_BEFORE_RELOAD_LOCAL_VERSION_MS = 1000;
@@ -57,6 +59,7 @@ public class MultisiteReplicationFetchFilter implements ReplicationFetchFilter {
         gitRepositoryManager.openRepository(Project.nameKey(projectName))) {
       RefDatabase refDb = repository.getRefDatabase();
       return refs.stream()
+          .filter(ref -> !hasBeenRemovedFromGlobalRefDb(projectName, ref))
           .filter(
               ref -> {
                 Optional<ObjectId> localRefOid =
@@ -79,6 +82,31 @@ public class MultisiteReplicationFetchFilter implements ReplicationFetchFilter {
       logger.atSevere().withCause(ioe).log(message);
       return Collections.emptySet();
     }
+  }
+
+  /* If the ref to fetch has been set to all zeros on the global-refdb, it means
+   * that whatever is the situation locally, we do not need to fetch it:
+   * - If the remote still has it, fetching it will be useless because the global
+   *   state is that the ref should be removed.
+   * - If the remote doesn't have it anymore, trying to fetch the ref won't do
+   *   anything because you can't just remove local refs by fetching.
+   */
+  private boolean hasBeenRemovedFromGlobalRefDb(String projectName, String ref) {
+    if (foundAsZeroInSharedRefDb(Project.nameKey(projectName), ref)) {
+      repLog.info(
+          "{}:{} is found as zeros (removed) in shared-refdb thus will NOT BE fetched",
+          projectName,
+          ref);
+      return true;
+    }
+    return false;
+  }
+
+  private boolean foundAsZeroInSharedRefDb(NameKey projectName, String ref) {
+    return sharedRefDb
+        .get(projectName, ref, String.class)
+        .map(r -> ZERO_ID_NAME.equals(r))
+        .orElse(false);
   }
 
   private Optional<ObjectId> getSha1IfUpToDateWithGlobalRefDb(
