181599ed21d67eb069d917fa374e72f80ac2e9cb
Daniele Sassoli <danielesassoli@gmail.com>
2023-05-30 21:22:22 +0000

    Create replication lag metric per repository
    Whenever a repository has a replication lag greater than zero report
    its specific metric using the repository name as a suffix.
    
    This enables the creation of repository-specific alerts when they get
    out of sync.
    
    Bug: Issue 16999
    Change-Id: If11e6576efffcae46d8464990a723824635465a2

---
+++ src/test/java/com/googlesource/gerrit/plugins/multisite/consumer/CallbackMetricMaker.java
@@ -0,0 +1,30 @@
+package com.googlesource.gerrit.plugins.multisite.consumer;
+
+import com.google.common.base.Supplier;
+import com.google.gerrit.extensions.registration.RegistrationHandle;
+import com.google.gerrit.metrics.Description;
+import com.google.gerrit.metrics.DisabledMetricMaker;
+
+public class CallbackMetricMaker extends DisabledMetricMaker {
+  private int callbackMetricCounter = 0;
+
+  public int getCallbackMetricCounter() {
+    return callbackMetricCounter;
+  }
+
+  @Override
+  public <V> RegistrationHandle newCallbackMetric(
+      String name, Class<V> valueClass, Description desc, Supplier<V> trigger) {
+    callbackMetricCounter += 1;
+    return new RegistrationHandle() {
+
+      @Override
+      public void remove() {
+      }
+    };
+  }
+
+  public void resetCallbackMetricCounter() {
+    callbackMetricCounter = 0;
+  }
+}
