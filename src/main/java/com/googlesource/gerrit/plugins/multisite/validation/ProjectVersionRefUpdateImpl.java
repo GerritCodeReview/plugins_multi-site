7eed388e90ee665ec6d161234df97f7074761f1f
Luca Milanesio <luca.milanesio@gmail.com>
2023-12-13 22:58:42 +0000

    Update the refs/multi-site/version/value once per batch-refupdate
    The update of refs/multi-site/version/value on the global-refdb
    is a very expensive operation for two reasons:
    - The global-refdb is a remote system, potentially with a high
      network latency
    - It needs to maintain global consistency across geographical areas
      which would also include sever-side synchronisation
    
    Reducing the number of updates on the global-refdb is paramount
    for keeping an acceptable performance in Gerrit updates.
    When multiple updates happen in the same batch for Gerrit, it would
    not make sense to update the global-refdb multiple times for the
    version value because globally the batch represents a unit of work
    and performing it as a whole means updating the version of the
    repository once.
    
    Move from individual stream-events to batch ref-update listener
    for updating the refs/multi-site/version/value, so that multiple
    updates would result in a single global-refdb update.
    Note that the batch ref-update listener is not a stream event
    and therefore the code does not need to check and test the
    instance-id anymore because all the events received are always
    local.
    
    For example, the upload of a new patch-set for a change was
    previously generating four different updates on the global-refdb
    for the refs/multi-site/version/value, whilst now there is only
    one single update generated for the  whole batch.
    
    Change-Id: Ia6c7f5aa085a8e2770b40df34145bb937cf1772e

--- src/main/java/com/googlesource/gerrit/plugins/multisite/validation/ProjectVersionRefUpdateImpl.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/ProjectVersionRefUpdateImpl.java
@@ -25,10 +25,8 @@ import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.Project.NameKey;
 import com.google.gerrit.entities.RefNames;
-import com.google.gerrit.server.config.GerritInstanceId;
+import com.google.gerrit.extensions.events.GitBatchRefUpdateListener;
 import com.google.gerrit.server.events.Event;
-import com.google.gerrit.server.events.EventListener;
-import com.google.gerrit.server.events.RefUpdatedEvent;
 import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.inject.Inject;
@@ -45,7 +43,8 @@ import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.Repository;
 
 @Singleton
-public class ProjectVersionRefUpdateImpl implements EventListener, ProjectVersionRefUpdate {
+public class ProjectVersionRefUpdateImpl
+    implements GitBatchRefUpdateListener, ProjectVersionRefUpdate {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
   private static final Set<RefUpdate.Result> SUCCESSFUL_RESULTS =
       ImmutableSet.of(RefUpdate.Result.NEW, RefUpdate.Result.FORCED, RefUpdate.Result.NO_CHANGE);
@@ -53,7 +52,6 @@ public class ProjectVersionRefUpdateImpl implements EventListener, ProjectVersio
   private final GitRepositoryManager gitRepositoryManager;
   private final GitReferenceUpdated gitReferenceUpdated;
   private final ProjectVersionLogger verLogger;
-  private final String nodeInstanceId;
 
   protected final SharedRefDatabaseWrapper sharedRefDb;
 
@@ -62,22 +60,17 @@ public class ProjectVersionRefUpdateImpl implements EventListener, ProjectVersio
       GitRepositoryManager gitRepositoryManager,
       SharedRefDatabaseWrapper sharedRefDb,
       GitReferenceUpdated gitReferenceUpdated,
-      ProjectVersionLogger verLogger,
-      @GerritInstanceId String nodeInstanceId) {
+      ProjectVersionLogger verLogger) {
     this.gitRepositoryManager = gitRepositoryManager;
     this.sharedRefDb = sharedRefDb;
     this.gitReferenceUpdated = gitReferenceUpdated;
     this.verLogger = verLogger;
-    this.nodeInstanceId = nodeInstanceId;
   }
 
   @Override
-  public void onEvent(Event event) {
-    logger.atFine().log("Processing event type: %s", event.type);
+  public void onGitBatchRefUpdate(Event event) {
     // Producer of the Event use RefUpdatedEvent to trigger the version update
-    if (nodeInstanceId.equals(event.instanceId) && event instanceof RefUpdatedEvent) {
-      updateProducerProjectVersionUpdate((RefUpdatedEvent) event);
-    }
+    updateProducerProjectVersionUpdate(event);
   }
 
   private boolean isSpecialRefName(String refName) {
@@ -86,17 +79,17 @@ public class ProjectVersionRefUpdateImpl implements EventListener, ProjectVersio
         || refName.equals(MULTI_SITE_VERSIONING_REF);
   }
 
-  private void updateProducerProjectVersionUpdate(RefUpdatedEvent refUpdatedEvent) {
-    String refName = refUpdatedEvent.getRefName();
-
-    if (isSpecialRefName(refName)) {
-      logger.atFine().log(
-          "Found a special ref name %s, skipping update for %s",
-          refName, refUpdatedEvent.getProjectNameKey().get());
-      return;
+  private void updateProducerProjectVersionUpdate(Event refUpdatedEvent) {
+    for (String refName : refUpdatedEvent.getRefNames()) {
+      if (isSpecialRefName(refName)) {
+        logger.atFine().log(
+            "Found a special ref name %s, skipping update for %s",
+            refName, refUpdatedEvent.getProjectName());
+        return;
+      }
     }
     try {
-      Project.NameKey projectNameKey = refUpdatedEvent.getProjectNameKey();
+      Project.NameKey projectNameKey = Project.nameKey(refUpdatedEvent.getProjectName());
       long newVersion = getCurrentGlobalVersionNumber();
 
       RefUpdate newProjectVersionRefUpdate = updateLocalProjectVersion(projectNameKey, newVersion);
@@ -109,7 +102,7 @@ public class ProjectVersionRefUpdateImpl implements EventListener, ProjectVersio
     } catch (LocalProjectVersionUpdateException | SharedProjectVersionUpdateException e) {
       logger.atSevere().withCause(e).log(
           "Issue encountered when updating version for project %s",
-          refUpdatedEvent.getProjectNameKey());
+          refUpdatedEvent.getProjectName());
     }
   }
 
