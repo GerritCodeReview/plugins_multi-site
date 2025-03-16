36eff93f31746eb1df6b0267d7a624730b7ec3a2
Luca Milanesio <luca.milanesio@gmail.com>
2025-03-16 11:58:30 +0000

    Implement filterAndLock in ReplicationFetchFilter
    The ReplicationFetchFilter allows to also lock refs whilst filtering
    them, which is paramount for preventing the refs to be fetch from being
    mutated and therefore causing potential split-brains.
    
    Lock local refs locally for allowing concurrent access to happen on
    other nodes that are not impacted by the local replication tasks.
    
    Change-Id: I0f8778dee71b3e3963403541b212058cdd42c784

---
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/ReentrantRefDbLocker.java
@@ -0,0 +1,74 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.multisite.validation;
+
+import com.gerritforge.gerrit.globalrefdb.RefDbLockException;
+import com.gerritforge.gerrit.globalrefdb.validation.RefLocker;
+import com.google.gerrit.entities.Project;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.multisite.Configuration;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class ReentrantRefDbLocker implements RefLocker {
+  private final ConcurrentHashMap<String, RefLock> refsLocks;
+  private final long timeoutMsec;
+
+  private class RefLock implements AutoCloseable {
+    private final Lock lock;
+
+    RefLock() {
+      lock = new ReentrantLock();
+    }
+
+    boolean lock() throws InterruptedException {
+      return lock.tryLock(timeoutMsec, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void close() throws Exception {
+      lock.unlock();
+    }
+  }
+
+  @Inject
+  public ReentrantRefDbLocker(Configuration configuration) {
+    this.timeoutMsec = configuration.localRefLockTimeoutMsec();
+    refsLocks = new ConcurrentHashMap<>();
+  }
+
+  @Override
+  public AutoCloseable lockRef(Project.NameKey project, String refName) throws RefDbLockException {
+    RefLock lock = refsLocks.computeIfAbsent(getKey(project, refName), ref -> new RefLock());
+    try {
+      if (lock.lock()) {
+        return lock;
+      }
+
+      throw new RefDbLockException(
+          project.get(),
+          refName,
+          String.format("Unable to acquire local ref lock after %s msec", timeoutMsec));
+    } catch (InterruptedException e) {
+      throw new RefDbLockException(project.get(), refName, e);
+    }
+  }
+
+  private String getKey(Project.NameKey project, String refName) {
+    return String.format("%s:%s", project.get(), refName);
+  }
+}
