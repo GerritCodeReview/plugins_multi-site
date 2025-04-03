4b219cdb176a839c4c9fe444d0aaf639e62473d0
Daniele Sassoli <danielesassoli@gmail.com>
2025-04-03 11:01:25 +0100

    Disable replicationLag by default
    Bug: Issue 408010451
    Change-Id: I921cc752d15c0189c154ea0ccfea1bb01a786551

--- src/main/java/com/googlesource/gerrit/plugins/multisite/Configuration.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/Configuration.java
@@ -119,12 +119,6 @@ public class Configuration {
                         REPLICATION_LAG_REFRESH_INTERVAL,
                         REPLICATION_LAG_REFRESH_INTERVAL_DEFAULT.toMillis(),
                         TimeUnit.MILLISECONDS)));
-    replicationLagEnabled =
-        memoize(
-            () ->
-                lazyMultiSiteCfg
-                    .get()
-                    .getBoolean(REF_DATABASE, null, REPLICATION_LAG_ENABLED, true));
 
     pushReplicationFilterEnabled =
         memoize(
@@ -138,6 +132,21 @@ public class Configuration {
                 lazyMultiSiteCfg
                     .get()
                     .getBoolean(REF_DATABASE, null, PULL_REPLICATION_FILTER_ENABLED, true));
+
+
+    replicationLagEnabled =
+        memoize(
+            () -> {
+              if (pullReplicationFilterEnabled.get()) {
+                return false;
+              } else {
+                return lazyMultiSiteCfg
+                    .get()
+                    .getBoolean(REF_DATABASE, null, REPLICATION_LAG_ENABLED, true);
+              }
+            });
+
+
     localRefLockTimeoutMsec =
         memoize(
             () ->
