0f22a75f9dc34a63dcb59ba4272ba5a215bab636
Luca Milanesio <luca.milanesio@gmail.com>
2025-06-19 23:57:07 +0100

    Manage change deletion performed by pull-replication
    When a delete change is replicated through the pull-replication plugin,
    the fetch-ref-replicated event contains the ref-name of the change /meta
    with a FORCED result and an inexistent ref on the repository.
    
    Leverage the event in the FetchRefReplicatedEventHandler and delete also
    the change from the index.
    
    Depends-On: https://gerrit-review.googlesource.com/c/plugins/replication/+/480184?
    Change-Id: I8567a583fc149eb630c72eb45084a5e7b423decb

--- src/test/java/com/googlesource/gerrit/plugins/multisite/index/FetchRefReplicatedEventHandlerTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/multisite/index/FetchRefReplicatedEventHandlerTest.java
@@ -16,6 +16,7 @@ package com.googlesource.gerrit.plugins.multisite.index;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -23,12 +24,14 @@ import static org.mockito.Mockito.verify;
 
 import com.google.gerrit.entities.Change;
 import com.google.gerrit.entities.Project;
+import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.index.change.ChangeIndexer;
 import com.googlesource.gerrit.plugins.replication.pull.Context;
 import com.googlesource.gerrit.plugins.replication.pull.FetchRefReplicatedEvent;
 import com.googlesource.gerrit.plugins.replication.pull.ReplicationState;
 import com.googlesource.gerrit.plugins.replication.pull.ReplicationState.RefFetchResult;
 import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.URIish;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,20 +39,46 @@ import org.junit.Test;
 public class FetchRefReplicatedEventHandlerTest {
   private static final String LOCAL_INSTANCE_ID = "local-instance-id";
   private static final String REMOTE_INSTANCE_ID = "remote-instance-id";
+  private static final Project.NameKey PROJECT_NAME_KEY = Project.nameKey("testProject");
   private ChangeIndexer changeIndexerMock;
+  private GitRepositoryManager gitRepositoryManager;
+  private Repository repository;
   private FetchRefReplicatedEventHandler fetchRefReplicatedEventHandler;
   private static URIish sourceUri;
 
   @Before
   public void setUp() throws Exception {
     changeIndexerMock = mock(ChangeIndexer.class);
+    gitRepositoryManager = mock(GitRepositoryManager.class);
+    repository = mock(Repository.class);
+    doReturn(repository).when(gitRepositoryManager).openRepository(any());
     fetchRefReplicatedEventHandler =
-        new FetchRefReplicatedEventHandler(changeIndexerMock, LOCAL_INSTANCE_ID);
+        new FetchRefReplicatedEventHandler(
+            changeIndexerMock, LOCAL_INSTANCE_ID, gitRepositoryManager);
     sourceUri = new URIish("git://aSourceNode/testProject.git");
   }
 
   @Test
   public void onEventShouldIndexExistingChange() {
+    String ref = "refs/changes/41/41/meta";
+    Change.Id changeId = Change.Id.fromRef(ref);
+    try {
+      Context.setLocalEvent(true);
+      fetchRefReplicatedEventHandler.onEvent(
+          newFetchRefReplicatedEvent(
+              PROJECT_NAME_KEY,
+              ref,
+              ReplicationState.RefFetchResult.SUCCEEDED,
+              LOCAL_INSTANCE_ID,
+              RefUpdate.Result.FAST_FORWARD));
+      verify(changeIndexerMock, times(1)).index(eq(PROJECT_NAME_KEY), eq(changeId));
+    } finally {
+      Context.unsetLocalEvent();
+    }
+  }
+
+  @Test
+  public void onEventShouldDeleteChangeFromIndex() {
     Project.NameKey projectNameKey = Project.nameKey("testProject");
     String ref = "refs/changes/41/41/meta";
     Change.Id changeId = Change.Id.fromRef(ref);
@@ -57,8 +86,12 @@ public class FetchRefReplicatedEventHandlerTest {
       Context.setLocalEvent(true);
       fetchRefReplicatedEventHandler.onEvent(
           newFetchRefReplicatedEvent(
-              projectNameKey, ref, ReplicationState.RefFetchResult.SUCCEEDED, LOCAL_INSTANCE_ID));
-      verify(changeIndexerMock, times(1)).index(eq(projectNameKey), eq(changeId));
+              projectNameKey,
+              ref,
+              ReplicationState.RefFetchResult.SUCCEEDED,
+              LOCAL_INSTANCE_ID,
+              RefUpdate.Result.FORCED));
+      verify(changeIndexerMock, times(1)).delete(eq(changeId));
     } finally {
       Context.unsetLocalEvent();
     }
@@ -71,7 +104,11 @@ public class FetchRefReplicatedEventHandlerTest {
     Change.Id changeId = Change.Id.fromRef(ref);
     fetchRefReplicatedEventHandler.onEvent(
         newFetchRefReplicatedEvent(
-            projectNameKey, ref, ReplicationState.RefFetchResult.SUCCEEDED, REMOTE_INSTANCE_ID));
+            projectNameKey,
+            ref,
+            ReplicationState.RefFetchResult.SUCCEEDED,
+            REMOTE_INSTANCE_ID,
+            RefUpdate.Result.FAST_FORWARD));
     verify(changeIndexerMock, never()).index(eq(projectNameKey), eq(changeId));
   }
 
@@ -82,7 +119,11 @@ public class FetchRefReplicatedEventHandlerTest {
     Change.Id changeId = Change.Id.fromRef(ref);
     fetchRefReplicatedEventHandler.onEvent(
         newFetchRefReplicatedEvent(
-            projectNameKey, ref, ReplicationState.RefFetchResult.SUCCEEDED, LOCAL_INSTANCE_ID));
+            projectNameKey,
+            ref,
+            ReplicationState.RefFetchResult.SUCCEEDED,
+            LOCAL_INSTANCE_ID,
+            RefUpdate.Result.FAST_FORWARD));
     verify(changeIndexerMock, never()).index(eq(projectNameKey), eq(changeId));
   }
 
@@ -93,7 +134,8 @@ public class FetchRefReplicatedEventHandlerTest {
             Project.nameKey("testProject"),
             "invalidRef",
             ReplicationState.RefFetchResult.SUCCEEDED,
-            LOCAL_INSTANCE_ID));
+            LOCAL_INSTANCE_ID,
+            RefUpdate.Result.FAST_FORWARD));
     verify(changeIndexerMock, never()).index(any(), any());
   }
 
@@ -103,7 +145,11 @@ public class FetchRefReplicatedEventHandlerTest {
     String ref = "refs/changes/41/41/meta";
     fetchRefReplicatedEventHandler.onEvent(
         newFetchRefReplicatedEvent(
-            projectNameKey, ref, ReplicationState.RefFetchResult.FAILED, LOCAL_INSTANCE_ID));
+            projectNameKey,
+            ref,
+            ReplicationState.RefFetchResult.FAILED,
+            LOCAL_INSTANCE_ID,
+            RefUpdate.Result.FAST_FORWARD));
     verify(changeIndexerMock, never()).index(any(), any());
   }
 
@@ -113,15 +159,23 @@ public class FetchRefReplicatedEventHandlerTest {
     String ref = "refs/changes/41/41/meta";
     fetchRefReplicatedEventHandler.onEvent(
         newFetchRefReplicatedEvent(
-            projectNameKey, ref, ReplicationState.RefFetchResult.NOT_ATTEMPTED, LOCAL_INSTANCE_ID));
+            projectNameKey,
+            ref,
+            ReplicationState.RefFetchResult.NOT_ATTEMPTED,
+            LOCAL_INSTANCE_ID,
+            RefUpdate.Result.FAST_FORWARD));
     verify(changeIndexerMock, never()).index(any(), any());
   }
 
   private FetchRefReplicatedEvent newFetchRefReplicatedEvent(
-      Project.NameKey projectNameKey, String ref, RefFetchResult fetchResult, String instanceId) {
+      Project.NameKey projectNameKey,
+      String ref,
+      RefFetchResult fetchResult,
+      String instanceId,
+      RefUpdate.Result refUpdateResult) {
     FetchRefReplicatedEvent event =
         new FetchRefReplicatedEvent(
-            projectNameKey.get(), ref, sourceUri, fetchResult, RefUpdate.Result.FAST_FORWARD);
+            projectNameKey.get(), ref, sourceUri, fetchResult, refUpdateResult);
     event.instanceId = instanceId;
     return event;
   }
