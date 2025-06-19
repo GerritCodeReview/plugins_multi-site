fa230feec6ac6223b6552c37499302f84f152d99
Luca Milanesio <luca.milanesio@gmail.com>
2025-06-19 23:57:07 +0100

    Manage change deletion performed by pull-replication
    When a delete change is replicated through the pull-replication plugin,
    the fetch-ref-replicated event contains the ref-name of the change /meta
    with a FORCED result and an inexistent ref on the repository.
    
    Leverage the event in the FetchRefReplicatedEventHandler and delete also
    the change from the index.
    
    Depends-On: https://gerrit-review.googlesource.com/c/plugins/replication/+/480184?
    Change-Id: I8567a583fc149eb630c72eb45084a5e7b423decb

--- src/main/java/com/googlesource/gerrit/plugins/multisite/index/FetchRefReplicatedEventHandler.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/index/FetchRefReplicatedEventHandler.java
@@ -21,21 +21,30 @@ import com.google.gerrit.entities.RefNames;
 import com.google.gerrit.server.config.GerritInstanceId;
 import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.events.EventListener;
+import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.index.change.ChangeIndexer;
 import com.google.inject.Inject;
 import com.googlesource.gerrit.plugins.multisite.forwarder.Context;
 import com.googlesource.gerrit.plugins.replication.pull.FetchRefReplicatedEvent;
 import com.googlesource.gerrit.plugins.replication.pull.ReplicationState;
+import java.io.IOException;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
 
 public class FetchRefReplicatedEventHandler implements EventListener {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private final GitRepositoryManager gitRepositoryManager;
   private ChangeIndexer changeIndexer;
   private final String instanceId;
 
   @Inject
-  FetchRefReplicatedEventHandler(ChangeIndexer changeIndexer, @GerritInstanceId String instanceId) {
+  FetchRefReplicatedEventHandler(
+      ChangeIndexer changeIndexer,
+      @GerritInstanceId String instanceId,
+      GitRepositoryManager gitRepositoryManager) {
     this.changeIndexer = changeIndexer;
     this.instanceId = instanceId;
+    this.gitRepositoryManager = gitRepositoryManager;
   }
 
   @Override
@@ -46,7 +55,14 @@ public class FetchRefReplicatedEventHandler implements EventListener {
 
       if (event instanceof FetchRefReplicatedEvent && isLocalEvent(event)) {
         FetchRefReplicatedEvent fetchRefReplicatedEvent = (FetchRefReplicatedEvent) event;
-        if (!RefNames.isNoteDbMetaRef(fetchRefReplicatedEvent.getRefName())
+        // This removal of the ':' prefix is needed because of a bug in the pull-replication plugin
+        // that may leave the deleted refs notification as a ref-spec instead of the deleted
+        // ref-name.
+        String refName =
+            fetchRefReplicatedEvent.ref.startsWith(":")
+                ? fetchRefReplicatedEvent.ref.substring(1)
+                : fetchRefReplicatedEvent.ref;
+        if (!RefNames.isNoteDbMetaRef(refName)
             || !fetchRefReplicatedEvent
                 .getStatus()
                 .equals(ReplicationState.RefFetchResult.SUCCEEDED.toString())) {
@@ -54,18 +70,44 @@ public class FetchRefReplicatedEventHandler implements EventListener {
         }
 
         Project.NameKey projectNameKey = fetchRefReplicatedEvent.getProjectNameKey();
-        logger.atFine().log(
-            "Indexing ref '%s' for project %s",
-            fetchRefReplicatedEvent.getRefName(), projectNameKey.get());
-        Change.Id changeId = Change.Id.fromRef(fetchRefReplicatedEvent.getRefName());
-        if (changeId != null) {
-          changeIndexer.index(projectNameKey, changeId);
-        } else {
-          logger.atWarning().log(
-              "Couldn't get changeId from refName. Skipping indexing of change %s for project %s",
-              fetchRefReplicatedEvent.getRefName(), projectNameKey.get());
+        Change.Id changeId = Change.Id.fromRef(refName);
+
+        try (Repository repo = gitRepositoryManager.openRepository(projectNameKey)) {
+          if (changeId != null) {
+            RefUpdate.Result fetchRefReplicatedEventResult =
+                fetchRefReplicatedEvent.getRefUpdateResult();
+            if ((RefUpdate.Result.FORCED.equals(fetchRefReplicatedEventResult)
+                    || RefUpdate.Result.NO_CHANGE.equals(fetchRefReplicatedEventResult))
+                && repo.exactRef(refName) == null) {
+              logger.atInfo().log(
+                  "Deleting change '%s' from index following the forced delete of '%s' on project"
+                      + " %s",
+                  changeId, refName, projectNameKey.get());
+              // NOTE: The change has been already deleted and we just don't know if it was a
+              // locally
+              // created change or imported, because the meta-data is not there anymore.
+              // In the future, it would be best to have that information passed to the event by the
+              // pull-replication plugin *before* the delete, and added to the
+              // FetchRefReplicatedEvent,
+              // so that multi-site can use it for computing the virtual change-id and assuring the
+              // delete
+              // of the imported changes as well.
+              changeIndexer.delete(changeId);
+            } else {
+              logger.atInfo().log(
+                  "Indexing change '%s' following update of '%s' on project %s",
+                  changeId, refName, projectNameKey.get());
+              changeIndexer.index(projectNameKey, changeId);
+            }
+          } else {
+            logger.atWarning().log(
+                "Couldn't get changeId from refName. Skipping indexing of change %s for project %s",
+                refName, projectNameKey.get());
+          }
         }
       }
+    } catch (IOException e) {
+      logger.atSevere().withCause(e).log("Unable to process event %s", event);
     } finally {
       Context.setForwardedEvent(isForwarded);
     }
