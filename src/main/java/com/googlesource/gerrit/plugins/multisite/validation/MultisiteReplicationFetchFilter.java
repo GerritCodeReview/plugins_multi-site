07be2a596a415e2b52bdce09f8c79184e46d1da6
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-22 16:26:05 -0700

    Revert "Do not filter out refs removals when filtering pull-replication fetches"
    This reverts commit 05960652371637b0e33ddae4005b1149bed5667a.
    
    Reason for revert: Created the regression 515801262.
    
    The test
    shouldFilterOutRefMissingInTheLocalRepositoryAndDeletedInSharedRefDb was
    introduced for fixing the Issue 40015453 where entries that were deleted
    in the global-refdb were never fetched.
    
    It was then in I368e64c47d2c because of the assumption that management
    of removal of refs via fetching would have let the Git protocol to
    resolve the correct alignment of deleted refs.
    
    Change-Id: I15b8587d2cb93bd378b5b0d62fd516acb3210d52

--- src/main/java/com/googlesource/gerrit/plugins/multisite/validation/MultisiteReplicationFetchFilter.java
+++ src/main/java/com/googlesource/gerrit/plugins/multisite/validation/MultisiteReplicationFetchFilter.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Sets;
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.entities.Project;
+import com.google.gerrit.entities.Project.NameKey;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -86,6 +87,29 @@ public class MultisiteReplicationFetchFilter extends AbstractMultisiteReplicatio
                             ref,
                             oid.getName()));
 
+                try {
+                  Ref localRef = refDb.exactRef(ref);
+                  repLog.info("{}:{} currently set to {}", projectName, ref, localRef == null ? "<null>" : localRef.getObjectId().getName());
+                  if (localRef == null
+                      && foundAsZeroInSharedRefDb(Project.nameKey(projectName), ref)) {
+                    repLog.info(
+                        "{}:{} was removed locally and is set to zeros in the shared-refdb and thus"
+                            + " will NOT BE fetched",
+                        projectName,
+                        ref);
+                    return false;
+                  }
+                } catch (IOException ioe) {
+                  String message =
+                      String.format(
+                          "Cannot dereference ref '%s' for project '%s' therefore will NOT BE"
+                              + " fetched",
+                          ref, projectName);
+                  repLog.error(message);
+                  logger.atSevere().withCause(ioe).log("%s", message);
+                  return false;
+                }
+
                 return !localRefOid.isPresent();
               })
           .collect(Collectors.toSet());
@@ -95,6 +119,13 @@ public class MultisiteReplicationFetchFilter extends AbstractMultisiteReplicatio
       logger.atSevere().withCause(ioe).log("%s", message);
       return Collections.emptySet();
     }
+  }
+
+  private boolean foundAsZeroInSharedRefDb(NameKey projectName, String ref) {
+    return sharedRefDb
+        .get(projectName, ref, String.class)
+        .map(r -> ZERO_ID_NAME.equals(r))
+        .orElse(false);
   }
 
   @Override
